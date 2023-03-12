27f5c9e4ebf3e9ad7d6fcfc8268307577dd2a737
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-03-12 13:34:30 -0300

    fix(nvim): Remove format on save hook

--- .config/nvim/lua/LSPSettings/NullLS.lua
+++ .config/nvim/lua/LSPSettings/NullLS.lua
@@ -20,30 +20,6 @@ local sources = {
   Diagnostics.eslint_d,
 }
 
-local LSPFormatting = function(bufnr)
-  vim.lsp.buf.format({
-    filter = function(client)
-      return client.name == "null-ls"
-    end,
-    bufnr = bufnr,
-  })
-end
-
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-local on_attach = function(client, bufnr)
-  if client.supports_method("textDocument/formatting") then
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        LSPFormatting(bufnr)
-      end,
-    })
-  end
-end
-
 NullLS.setup({
   sources = sources,
   on_attach = on_attach,
