941d78596dbcbb95e7654d820ea246c41b13d56d
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-10 11:34:53 -0300

    fix(fish): Custom keybindings conflicting with `fish` defaults.

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -5,10 +5,10 @@ function fish_user_key_bindings
   bind \cr fzf_history
 
   # 'TMUX' + 'FZF'
-  bind \ca tmux_attach
-  bind \ck tmux_kill
+  bind \ea tmux_attach
+  bind \ek tmux_kill
 
-  # Bring bash's "!!" and "!$! to fish. Depends on the two functions above.
+  # Brings bash's "!!" and "!$" to fish.
   bind ! history_previous_command
   bind '$' history_previous_command_arguments
 end
