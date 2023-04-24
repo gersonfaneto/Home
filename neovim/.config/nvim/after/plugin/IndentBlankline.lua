4ebb7de59f1fac59dc58fe7c05bae6431d832ed7
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-04-24 09:12:25 -0300

    feat(nvim): Setup colors for `indent-blankline`.

--- neovim/.config/nvim/after/plugin/IndentBlankline.lua
+++ neovim/.config/nvim/after/plugin/IndentBlankline.lua
@@ -1,8 +1,25 @@
 local pCall = require("Utils.ProtectedCall")
+local vCMD = vim.cmd
 
 local IndentBlankline = pCall("indent_blankline")
+
+vCMD("highlight IndentBlanklineIndent1 guifg=#FA7970 gui=nocombine")
+vCMD("highlight IndentBlanklineIndent2 guifg=#FAA356 gui=nocombine")
+vCMD("highlight IndentBlanklineIndent3 guifg=#7CE38B gui=nocombine")
+vCMD("highlight IndentBlanklineIndent4 guifg=#77BDFB gui=nocombine")
+vCMD("highlight IndentBlanklineIndent5 guifg=#A2D2FB gui=nocombine")
+vCMD("highlight IndentBlanklineIndent6 guifg=#CEA5FB gui=nocombine")
 
 IndentBlankline.setup({
   show_trailing_blankline_indent = false,
   char = "┊",
+  char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
+  },
 })
+
