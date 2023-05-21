33b15f5264c4f42729ccff709fcd01e1f5c9c85b
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-05-20 22:12:31 -0300

    feat(wezterm): Override default colors.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -19,4 +19,33 @@ config.window_padding = {
 	bottom = 10,
 }
 
+config.colors = {
+  foreground = "#b3b1ad",
+  background = "#0d1117",
+
+  selection_fg = "#b3b1ad",
+  selection_bg = "#163356",
+
+  ansi = {
+    "#21262d",
+    "#ffa198",
+    "#56d364",
+    "#e3b341",
+    "#79c0ff",
+    "#d2a8ff",
+    "#56d4dd",
+    "#f0f6fc",
+  },
+  brights = {
+    "#6e7681",
+    "#fa7970",
+    "#7ce38b",
+    "#faa356",
+    "#77bdfb",
+    "#cea5fb",
+    "#a2d2fb",
+    "#ecf2f8",
+  },
+}
+
 return config
