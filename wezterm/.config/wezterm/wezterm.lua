4580d96ec2aa5ec364f8b25b1c209de546382e24
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-06-02 20:28:53 -0300

    feat(wezterm): Added new mapping and minor refactor.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,56 +1,63 @@
 local wezterm = require("wezterm")
-local config = {}
 
-config.color_scheme = "GitHub Dark"
+return {
+	color_scheme = "GitHub Dark",
 
-config.font = wezterm.font("Comic Mono")
-config.font_size = 14
+	font = wezterm.font("Comic Mono"),
+	font_size = 14,
 
-config.default_prog = { "/usr/bin/fish" }
+	default_prog = { "/usr/bin/fish" },
 
-config.window_decorations = "NONE"
-config.hide_tab_bar_if_only_one_tab = true
-config.hide_mouse_cursor_when_typing = true
+	window_decorations = "NONE",
+	hide_tab_bar_if_only_one_tab = true,
+	hide_mouse_cursor_when_typing = true,
 
-config.cursor_blink_rate = 400
-config.cursor_blink_ease_in = "Constant"
-config.cursor_blink_ease_out = "Constant"
-config.default_cursor_style = "BlinkingBar"
+	keys = {
+		{
+			key = "F11",
+			mods = "NONE",
+			action = wezterm.action.ToggleFullScreen,
+		},
+	},
 
-config.window_padding = {
-	left = 10,
-	right = 10,
-	top = 10,
-	bottom = 10,
+	cursor_blink_rate = 400,
+	cursor_blink_ease_in = "Constant",
+	cursor_blink_ease_out = "Constant",
+	default_cursor_style = "BlinkingBar",
+
+	window_padding = {
+		left = 10,
+		right = 10,
+		top = 10,
+		bottom = 10,
+	},
+
+	colors = {
+		foreground = "#b3b1ad",
+		background = "#0d1117",
+
+		selection_fg = "#b3b1ad",
+		selection_bg = "#163356",
+
+		ansi = {
+			"#21262d",
+			"#ffa198",
+			"#56d364",
+			"#e3b341",
+			"#79c0ff",
+			"#d2a8ff",
+			"#56d4dd",
+			"#f0f6fc",
+		},
+		brights = {
+			"#6e7681",
+			"#fa7970",
+			"#7ce38b",
+			"#faa356",
+			"#77bdfb",
+			"#cea5fb",
+			"#a2d2fb",
+			"#ecf2f8",
+		},
+	},
 }
-
-config.colors = {
-  foreground = "#b3b1ad",
-  background = "#0d1117",
-
-  selection_fg = "#b3b1ad",
-  selection_bg = "#163356",
-
-  ansi = {
-    "#21262d",
-    "#ffa198",
-    "#56d364",
-    "#e3b341",
-    "#79c0ff",
-    "#d2a8ff",
-    "#56d4dd",
-    "#f0f6fc",
-  },
-  brights = {
-    "#6e7681",
-    "#fa7970",
-    "#7ce38b",
-    "#faa356",
-    "#77bdfb",
-    "#cea5fb",
-    "#a2d2fb",
-    "#ecf2f8",
-  },
-}
-
-return config
