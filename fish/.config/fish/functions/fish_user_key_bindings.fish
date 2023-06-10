0acb7ec9debfa430149a921a882f303fa5176bc6
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-06-10 09:54:58 -0300

    feat(fish): Update mappings.

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -3,8 +3,8 @@ function fish_user_key_bindings
   bind \cf fzf_file
   bind \cr fzf_history
 
-  bind \ea tmux_attach
-  bind \ek tmux_kill
+  bind \ca tmux_attach
+  bind \ck tmux_kill
 
   bind ! history_previous_command
   bind '$' history_previous_command_arguments
