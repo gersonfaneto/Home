bccd6e20678076056858bbf82de8430ba05ba62a
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-06-30 21:28:23 -0300

    feat(wezterm): Add `catppuccin` option to color schemes.

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,14 +1,19 @@
+---@diagnostic disable: unused-local
+
 local wezterm = require("wezterm")
 
+local Gruvbox = "Gruvbox Material (Gogh)"
+local Catppuccin = "Catppuccin Mocha"
+
 return {
-	color_scheme = "Gruvbox Material (Gogh)",
+	color_scheme = Catppuccin,
 
 	colors = {
 		background = "#0d1117",
 	},
 
 	font = wezterm.font("Comic Mono"),
-	font_size = 14,
+	font_size = 12,
 
 	cursor_blink_rate = 400,
 	cursor_blink_ease_in = "Constant",
@@ -21,7 +26,7 @@ return {
 		top = 10,
 		bottom = 10,
 	},
-  window_background_opacity = 0.7,
+  window_background_opacity = 0.0,
 
 	window_decorations = "NONE",
 	hide_tab_bar_if_only_one_tab = true,
