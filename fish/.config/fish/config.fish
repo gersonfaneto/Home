f7c7401ad51c7819359a1e64c8008d54c52d1cb3
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-07-05 19:36:51 -0300

    feat(fish): Updated `$FZF_DEFAULT_OPTS` with `Rose Pine` colors.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -96,20 +96,20 @@ end
 
 if type -q fzf
   # Rose Pine
-  export FZF_DEFAULT_OPTS="--layout=reverse --height=50% --border=rounded \
-  --prompt='Search: ' --info=inline --multi --tabstop=2 --color=dark \
-	--color=fg:#908caa,bg:-1,hl:#ebbcba
-	--color=fg+:#e0def4,bg+:-1,hl+:#ebbcba
-	--color=border:#403d52,header:#31748f,gutter:#191724
-	--color=spinner:#f6c177,info:#9ccfd8,separator:#403d52
-	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
-
-  # Catppuccin
   # export FZF_DEFAULT_OPTS="--layout=reverse --height=50% --border=rounded \
   # --prompt='Search: ' --info=inline --multi --tabstop=2 --color=dark \
-  # --color=bg+:-1,bg:-1,spinner:#f5e0dc,hl:#f38ba8 \
-  # --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-  # --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+	# --color=fg:#908caa,bg:-1,hl:#ebbcba
+	# --color=fg+:#e0def4,bg+:-1,hl+:#ebbcba
+	# --color=border:#403d52,header:#31748f,gutter:#191724
+	# --color=spinner:#f6c177,info:#9ccfd8,separator:#403d52
+	# --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
+
+  # Catppuccin
+  export FZF_DEFAULT_OPTS="--layout=reverse --height=50% --border=rounded \
+  --prompt='Search: ' --info=inline --multi --tabstop=2 --color=dark \
+  --color=bg+:-1,bg:-1,spinner:#f5e0dc,hl:#f38ba8 \
+  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+  --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
   # Gruvbox
   # export FZF_DEFAULT_OPTS="--layout=reverse --height=50% --border=rounded \
