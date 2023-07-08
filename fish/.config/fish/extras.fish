e0bc5e201d68a2837c80053464b0bfe4b6a5934a
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-07-08 20:41:18 -0300

    fix(fish): Added missing source for `asdf`.

--- fish/.config/fish/extras.fish
+++ fish/.config/fish/extras.fish
@@ -6,6 +6,10 @@ if type -q zoxide
   zoxide init fish | source
 end
 
+if test -d "$HOME/.asdf"
+  source $HOME/.asdf/asdf.fish
+end
+
 if status is-interactive  
   and not set -q TMUX
     if not tmux has-session -t Home 2>/dev/null
