c1e696edc54c23661d685238d694e59cb2d0c7ba
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-07-14 23:19:48 -0300

    fix(fish): Added missing export for `$LD_LIBRARY_PATH`.

--- fish/.config/fish/exports.fish
+++ fish/.config/fish/exports.fish
@@ -6,6 +6,10 @@ if test -d "$HOME/.cargo/bin/"
   export PATH="$PATH:$HOME/.cargo/bin/"
 end
 
+if test -d "/usr/local/lib"
+  export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
+end
+
 if type -q bat
   export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 end
