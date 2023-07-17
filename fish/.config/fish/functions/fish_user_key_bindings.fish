55137eccaa7e8adae234a8446d15e5fc798225ed
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-07-17 15:14:37 -0300

    fix(fish): Updated mappings on `fish_user_key_bindings`.

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,11 +1,11 @@
 function fish_user_key_bindings
-  bind \cj fzf_directory
-  bind \cf fzf_file
-  bind \cr fzf_history
-  bind \ct fzf_theme
+  bind \ej fzf_directory
+  bind \ef fzf_file
+  bind \er fzf_history
+  bind \et fzf_theme
 
-  bind \ca tmux_attach
-  bind \ck tmux_kill
+  bind \ea tmux_attach
+  bind \ek tmux_kill
 
   bind ! history_previous_command
   bind '$' history_previous_command_arguments
