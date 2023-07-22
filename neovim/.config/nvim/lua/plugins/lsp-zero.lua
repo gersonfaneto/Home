5d8188d1f20690d9726a3604ee71432c955f3d3c
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-07-21 21:06:51 -0300

    feat(neovim): Added entry/configurations for `gopls`.

--- neovim/.config/nvim/lua/plugins/lsp-zero.lua
+++ neovim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -40,6 +40,7 @@ return {
         "csharp_ls",
         "cssls",
         "emmet_ls",
+        "gopls",
         "html",
         "jdtls",
         "lua_ls",
@@ -219,6 +220,45 @@ return {
             },
           },
         },
+      })
+
+      lspconfig.gopls.setup({
+        settings = {
+          gopls = {
+            gofumpt = true,
+            codelenses = {
+              gc_details = false,
+              generate = true,
+              regenerate_cgo = true,
+              run_govulncheck = true,
+              test = true,
+              tidy = true,
+              upgrade_dependency = true,
+              vendor = true,
+            },
+            hints = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
+            analyses = {
+              fieldalignment = true,
+              nilness = true,
+              unusedparams = true,
+              unusedwrite = true,
+              useany = true,
+            },
+            usePlaceholders = true,
+            completeUnimported = true,
+            staticcheck = true,
+            directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+            semanticTokens = true,
+          }
+        }
       })
 
       lspconfig.rust_analyzer.setup({
