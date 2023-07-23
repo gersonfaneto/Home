035482dcd0b2c943b92d24fc51755573714e9e07
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-07-22 22:18:20 -0300

    refactor(neovim): Moved `nvim-cmp` related settings to separate file.

--- neovim/.config/nvim/lua/plugins/lsp-zero.lua
+++ neovim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -13,23 +13,11 @@ return {
           "williamboman/mason-lspconfig.nvim",
         },
       },
-      {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-          "hrsh7th/cmp-path",
-          "hrsh7th/cmp-emoji",
-          "hrsh7th/cmp-buffer",
-          "hrsh7th/cmp-nvim-lsp",
-          "L3MON4D3/LuaSnip",
-          "saadparwaiz1/cmp_luasnip",
-        },
-      },
-      "neovim/nvim-lspconfig",
       "onsails/lspkind.nvim",
-      "b0o/schemastore.nvim",
+      "neovim/nvim-lspconfig",
     },
     config = function()
-      local lsp = require("lsp-zero").preset("recommended")
+      local lsp = require("lsp-zero").preset("lsp-only")
 
       local settings = require("core.settings")
 
@@ -165,8 +153,10 @@ return {
             timeout_ms = 10000,
           },
           servers = {
+            ["clangd"] = { "c", "cpp" },
             ["gopls"] = { "go" },
             ["lua_ls"] = { "lua" },
+            ["pyright"] = { "python" },
             ["rust_analyzer"] = { "rust" },
           },
         })
@@ -222,6 +212,35 @@ return {
         },
       })
 
+      lspconfig.rust_analyzer.setup({
+        settings = {
+          ["rust-analyzer"] = {
+            lens = {
+              enable = true,
+            },
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              runBuildScripts = true,
+            },
+            check = {
+              enable = true,
+              allFeatures = true,
+              command = "clippy",
+              extraArgs = { "--no-deps" },
+            },
+            procMacro = {
+              enable = true,
+              ignored = {
+                ["napi-derive"] = { "napi" },
+                ["async-trait"] = { "async_trait" },
+                ["async-recursion"] = { "async_recursion" },
+              },
+            },
+          },
+        },
+      })
+
       lspconfig.gopls.setup({
         settings = {
           gopls = {
@@ -261,222 +280,11 @@ return {
         }
       })
 
-      lspconfig.rust_analyzer.setup({
-        settings = {
-          ["rust-analyzer"] = {
-            lens = {
-              enable = true,
-            },
-            cargo = {
-              allFeatures = true,
-              loadOutDirsFromCheck = true,
-              runBuildScripts = true,
-            },
-            check = {
-              enable = true,
-              allFeatures = true,
-              command = "clippy",
-              extraArgs = { "--no-deps" },
-            },
-            procMacro = {
-              enable = true,
-              ignored = {
-                ["napi-derive"] = { "napi" },
-                ["async-trait"] = { "async_trait" },
-                ["async-recursion"] = { "async_recursion" },
-              },
-            },
-          },
-        },
-      })
-
       lsp.skip_server_setup({
         "jdtls",
       })
 
       lsp.setup()
-
-      local utils = require("utils.api")
-      local icons = require("utils.interface").icons
-
-      local cmp = require("cmp")
-      local cmp_types = require("cmp.types.cmp")
-      local cmp_mapping = require("cmp.config.mapping")
-
-      local luasnip = require("luasnip")
-      local lspkind = require("lspkind")
-
-      cmp.setup({
-        formatting = {
-          fields = { "kind", "abbr", "menu" },
-          format = function(entry, vim_item)
-            local max_width = 0
-            if max_width ~= 0 and #vim_item.abbr > max_width then
-              vim_item.abbr = string.sub(vim_item.abbr, 1, max_width - 1) .. icons.ui.Ellipsis
-            end
-            vim_item.kind = lspkind.presets.default[vim_item.kind] .. " " .. vim_item.kind
-
-            if entry.source.name == "copilot" then
-              vim_item.kind = icons.git.Octoface
-              vim_item.kind_hl_group = "CmpItemKindCopilot"
-            end
-
-            if entry.source.name == "emoji" then
-              vim_item.kind = icons.misc.Smiley
-              vim_item.kind_hl_group = "CmpItemKindEmoji"
-            end
-            vim_item.menu = ({
-              nvim_lsp = "[LSP]",
-              emoji = "[Emoji]",
-              path = "[Path]",
-              luasnip = "[Snippet]",
-              buffer = "[Buffer]",
-              copilot = "[Copilot]",
-            })[entry.source.name]
-            vim_item.dup = ({
-              buffer = 1,
-              path = 1,
-              nvim_lsp = 0,
-              luasnip = 1,
-            })[entry.source.name] or 0
-            return vim_item
-          end,
-        },
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered({
-            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-          }),
-          documentation = cmp.config.window.bordered({
-            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-          }),
-        },
-        sources = {
-          {
-            name = "copilot",
-            keyword_length = 5,
-            max_item_count = 3,
-            trigger_characters = {
-              {
-                ".",
-                ":",
-                "(",
-                "'",
-                '"',
-                "[",
-                ",",
-                "#",
-                "*",
-                "@",
-                "|",
-                "=",
-                "-",
-                "{",
-                "/",
-                "\\",
-                "+",
-                "?",
-                " ",
-              },
-            },
-          },
-          {
-            name = "nvim_lsp",
-            entry_filter = function(entry, ctx)
-              local kind = require("cmp.types.lsp").CompletionItemKind[entry:get_kind()]
-              if kind == "Snippet" and ctx.prev_context.filetype == "java" then
-                return false
-              end
-              if kind == "Text" then
-                return false
-              end
-              return true
-            end,
-          },
-
-          { name = "luasnip" },
-          { name = "path" },
-          { name = "buffer" },
-          { name = "emoji" },
-          { name = "crates" },
-          { name = "nvim_lua" },
-        },
-        mapping = cmp_mapping.preset.insert({
-          ["<C-k>"] = cmp_mapping(cmp_mapping.select_prev_item(), { "i", "c" }),
-          ["<C-j>"] = cmp_mapping(cmp_mapping.select_next_item(), { "i", "c" }),
-          ["<Down>"] = cmp_mapping(cmp_mapping.select_next_item({ behavior = cmp_types.SelectBehavior.Select }), {
-            "i",
-          }),
-          ["<Up>"] = cmp_mapping(cmp_mapping.select_prev_item({ behavior = cmp_types.SelectBehavior.Select }), {
-            "i",
-          }),
-          ["<C-b>"] = cmp_mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp_mapping.scroll_docs(4),
-          ["<C-y>"] = cmp_mapping({
-            i = cmp_mapping.confirm({ behavior = cmp_types.ConfirmBehavior.Replace, select = false }),
-            c = function(fallback)
-              if cmp.visible() then
-                cmp.confirm({ behavior = cmp_types.ConfirmBehavior.Replace, select = false })
-              else
-                fallback()
-              end
-            end,
-          }),
-          ["<Tab>"] = cmp_mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            elseif utils.jumpable(1) then
-              luasnip.jump(1)
-            elseif utils.has_words_before() then
-              -- cmp.complete()
-              fallback()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp_mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<C-c>"] = cmp_mapping.complete(),
-          ["<C-e>"] = cmp_mapping.abort(),
-          ["<CR>"] = cmp_mapping(function(fallback)
-            if cmp.visible() then
-              local confirm_opts = vim.deepcopy({
-                behavior = cmp_types.ConfirmBehavior.Replace,
-                select = false,
-              })
-              local is_insert_mode = function()
-                return vim.api.nvim_get_mode().mode:sub(1, 1) == "i"
-              end
-              if is_insert_mode() then
-                confirm_opts.behavior = cmp_types.ConfirmBehavior.Insert
-              end
-              local entry = cmp.get_selected_entry()
-              local is_copilot = entry and entry.source.name == "copilot"
-              if is_copilot then
-                confirm_opts.behavior = cmp_types.ConfirmBehavior.Replace
-                confirm_opts.select = true
-              end
-              if cmp.confirm(confirm_opts) then
-                return
-              end
-            end
-            fallback()
-          end),
-        }),
-      })
     end,
   },
 }
