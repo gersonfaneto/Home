ad7e11233025a10d278a388fdc3779ae6baf1d53
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-07-26 09:45:52 -0300

    feat(neovim): Added new `usercmd` - `NewNote`.

--- neovim/.config/nvim/lua/core/after/usercmd.lua
+++ neovim/.config/nvim/lua/core/after/usercmd.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_user_command("BufferDelete", function()
   local modified = vim.api.nvim_buf_get_option(0, "modified")
 
   if file_exists == 0 and modified then
-    local user_choice = vim.fn.input("The file is not saved, whether to force delete? Press enter or input [y/n]:")
+    local user_choice = vim.fn.input("The file is not saved, whether to force delete? [Y/n]: ")
     if user_choice == "y" or user_choice:len() == 0 then
       vim.cmd("bd!")
     end
@@ -16,4 +16,10 @@ vim.api.nvim_create_user_command("BufferDelete", function()
   local force = not vim.bo.buflisted or vim.bo.buftype == "nofile"
 
   vim.cmd(force and "bd!" or ("bp | bd! %s"):format(vim.api.nvim_get_current_buf()))
-end, { desc = "Delete the current Buffer while maintaining the window layout" })
+end, { desc = "Delete the current Buffer while maintaining the window layout." })
+
+vim.api.nvim_create_user_command("NewNote", function()
+  vim.ui.input({ prompt = "Name: ", relative = "editor" }, function(name)
+    vim.api.nvim_command(":e ~/Notes/" .. name .. ".md")
+  end)
+end, { desc = "Create a new note as a markdown file." })
