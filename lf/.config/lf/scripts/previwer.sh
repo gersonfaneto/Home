4d4eb2d85e3e89c5a97e1958cc0e809186f5c3eb
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-07-31 08:09:51 -0300

    feat(lf): Updated mappings.

--- lf/.config/lf/scripts/previwer.sh
+++ lf/.config/lf/scripts/previwer.sh
@@ -7,34 +7,35 @@ H=$3
 X=$4
 Y=$5
 
-FILE="$1"; shift
+FILE="$1"
+shift
 
 THUMBNAIL="$(mktemp)/thumbnail.png"
 
 preview() {
-  kitty +kitten icat --silent --stdin no --transfer-mode file --place "${W}x${H}@${X}x${Y}" "$FILE" < /dev/null > /dev/tty
+	kitty +kitten icat --silent --stdin no --transfer-mode file --place "${W}x${H}@${X}x${Y}" "$FILE" </dev/null >/dev/tty
 }
 
 case "$(basename "$FILE" | tr '[A-Z]' '[a-z]')" in
-  *.tar*) tar tf "$FILE" ;;
-  *.zip) unzip -l "$FILE" ;;
-  *.rar) unrar l "$FILE" ;;
-  *.7z) 7z l "$FILE" ;;
-  *.jpg|*.jpeg|*.png|*.bmp)
-    preview "$FILE"
-    ;;
-  *.svg)
-    convert "$FILE" "$THUMBNAIL"
-    preview "$THUMBNAIL"
-    ;;
-  *.avi|*.mp4|*.mkv)
-    ffmpeg -y -i "$FILE" -vframes 1 "$THUMBNAIL"
-    preview "$THUMBNAIL"
-    ;;
-  *)
-    bat --force-colorization --paging=never --style=changes,numbers \
-      --terminal-width $(($W - 3)) "$FILE" && false
-    ;;
+*.tar*) tar tf "$FILE" ;;
+*.zip) unzip -l "$FILE" ;;
+*.rar) unrar l "$FILE" ;;
+*.7z) 7z l "$FILE" ;;
+*.jpg | *.jpeg | *.png | *.bmp)
+	preview "$FILE"
+	;;
+*.svg)
+	convert "$FILE" "$THUMBNAIL"
+	preview "$THUMBNAIL"
+	;;
+*.avi | *.mp4 | *.mkv)
+	ffmpeg -y -i "$FILE" -vframes 1 "$THUMBNAIL"
+	preview "$THUMBNAIL"
+	;;
+*)
+	bat --force-colorization --paging=never --style=changes,numbers \
+		--terminal-width $((W - 3)) "$FILE" && false
+	;;
 esac
 
 return 127
