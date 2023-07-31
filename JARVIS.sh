0dd98458dc51061d3754778d935f56c1436f0d3b
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-07-31 08:24:53 -0300

    refactor: Minor adjustments to `JARVIS`.

--- JARVIS.sh
+++ JARVIS.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
 # Some colors for better progress visualization.
+NOTE="[\e[1;36mNOTE\e[0m]"
+OK="[\e[1;32mOK\e[0m]"
+ERROR="[\e[1;31mERROR\e[0m]"
+# ATTENTION="[\e[1;33mATTENTION\e[0m]"
+# WARNING="[\e[1;35mWARNING\e[0m]"
+# ACTION="[\e[1;33mACTION\e[0m]"
 
-M_NOTE="[\e[1;36mNOTE\e[0m]"
-M_OK="[\e[1;32mOK\e[0m]"
-M_ERROR="[\e[1;31mERROR\e[0m]"
-# M_ATT="[\e[1;33mATTENTION\e[0m]"
-# M_WRN="[\e[1;35mWARNING\e[0m]"
-# M_ACT="[\e[1;33mACTION\e[0m]"
-
-M_WAIT="\e[1A\e[K"
+WAIT="\e[1A\e[K"
 
 # Path to the installation log.
-INST_LOG="INSTALL.log"
+LOG="INSTALL.log"
 
 # List of directories to be excluded/used on the configuration linking step.
 EXCLUDE=("JARVIS.sh" "INSTALL.log" "README.md" "LICENSE" "fonts")
@@ -59,18 +58,18 @@ display_progress() {
 # Tests if a package is already installed and if not found it will attempt to install it.
 install_package() {
 	if yay -Q "$1" &>>/dev/null; then
-		echo -e "$M_OK - $1 is already installed."
+		echo -e "$OK - $1 is already installed."
 	else
-		echo -en "$M_NOTE - Now installing $1 ."
+		echo -en "$NOTE - Now installing $1 ."
 
-		yay -S --noconfirm "$1" &>>"$INST_LOG" &
+		yay -S --noconfirm "$1" &>>"$LOG" &
 
 		display_progress $!
 
 		if yay -Q "$1" &>>/dev/null; then
-			echo -e "$M_WAIT$M_OK - $1 was installed."
+			echo -e "$WAIT$OK - $1 was installed."
 		else
-			echo -e "$M_WAIT$M_ERROR - $1 install has failed, please check the log at $INST_LOG."
+			echo -e "$WAIT$ERROR - $1 install has failed, please check the log at $LOG."
 			exit
 		fi
 	fi
@@ -79,49 +78,47 @@ install_package() {
 clear
 
 # Let the user know that we will use sudo.
-cat <<EOM
-$(echo -e "$M_NOTE - This script will run some commands that require sudo.")
-$(echo -e "$M_NOTE - You will be prompted to enter your password when needed.")
-$(echo -e "$M_NOTE - If you are worried about entering your password then you may want to review the content of the script.")
-EOM
+echo -e "$NOTE - This script will run some commands that require sudo."
+echo -e "$NOTE - You will be prompted to enter your password when needed."
+echo -e "$NOTE - If you are worried about entering your password then you may want to review the content of the script."
 
-sleep 0.5
+sleep 1
 
 # Expect user input to confirm and continua with the script or exit.
 read -rep $'[\e[1;33mACTION\e[0m] - Would you like to continue with the install [Y/n] ' REPLY
 
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
-	echo -e "$M_NOTE - Setup starting..."
+	echo -e "$NOTE - Setup starting..."
 else
-	echo -e "$M_NOTE - This script will now exit, no changes were made to your system."
+	echo -e "$NOTE - This script will now exit, no changes were made to your system."
 	exit
 fi
 
 # Check for package manager (YAY).
 if [ ! -f /sbin/yay ]; then
-	echo -en "$M_NOTE - Configuring yay..."
+	echo -en "$NOTE - Configuring yay..."
 
-	git clone https://aur.archlinux.org/yay.git &>>"$INST_LOG"
+	git clone https://aur.archlinux.org/yay.git &>>"$LOG"
 
 	cd yay || exit
 
-	makepkg -si --noconfirm &>>../"$INST_LOG" &
+	makepkg -si --noconfirm &>>../"$LOG" &
 
 	display_progress $!
 
 	if [ -f /sbin/yay ]; then
-		echo -e "$M_WAIT$M_OK - yay configured with success."
+		echo -e "$WAIT$OK - yay configured with success."
 		cd ..
 
-		echo -en "$M_NOTE - Updating yay database."
+		echo -en "$NOTE - Updating yay database."
 
-		yay -Suy --noconfirm &>>"$INST_LOG" &
+		yay -Suy --noconfirm &>>"$LOG" &
 
 		display_progress $!
 
-		echo -e "$M_WAIT$M_OK - yay database updated with success."
+		echo -e "$WAIT$OK - yay database updated with success."
 	else
-		echo -e "$M_WAIT$M_ERROR - yay installation failed, please check the log at $INST_LOG!"
+		echo -e "$WAIT$ERROR - yay installation failed, please check the log at $LOG!"
 		exit
 	fi
 fi
@@ -130,7 +127,7 @@ fi
 read -rep $'[\e[1;33mACTION\e[0m] - Would you like to install the packages? [Y/n] ' REPLY
 
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
-	echo -e "$M_NOTE - Beggining installation, this may take a while..."
+	echo -e "$NOTE - Beggining installation, this may take a while..."
 
 	for PACKAGE in "${packages[@]}"; do
 		install_package "$PACKAGE"
@@ -141,22 +138,22 @@ fi
 read -rep $'[\e[1;33mACTION\e[0m] - Would you like to install asdf? [Y/n] ' REPLY
 
 if [[ -d "$HOME"/.asdf/ ]]; then
-	echo -e "$M_NOTE - asdf is already installed!"
+	echo -e "$NOTE - asdf is already installed!"
 elif [[ $REPLY =~ ^[Yy]$ || -z $REPLY ]]; then
-	echo -en "$M_NOTE - Beggining installation, this may take a while..."
+	echo -en "$NOTE - Beggining installation, this may take a while..."
 
-	git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0 &>>"$INST_LOG" &
+	git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.12.0 &>>"$LOG" &
 
 	display_progress $!
 
 	if [[ -d $HOME/.asdf/ ]]; then
-		echo -e "$M_WAIT$M_OK - asdf installed completed with success!"
+		echo -e "$WAIT$OK - asdf installed completed with success!"
 
 		mkdir -p ~/.config/fish/completions
 
 		ln -sf ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 	else
-		echo -e "$M_WAIT$M_ERROR - asdf installation failed, please check the log at $INST_LOG!"
+		echo -e "$WAIT$ERROR - asdf installation failed, please check the log at $INST_LOG!"
 		exit
 	fi
 fi
@@ -165,24 +162,24 @@ fi
 read -rep $'[\e[1;33mACTION\e[0m] - Would you like to link the configuration files? [Y/n] ' REPLY
 
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
-	echo -e "$M_NOTE - Linking configuration files..."
+	echo -e "$NOTE - Linking configuration files..."
 
 	# Setup each package configuration folder.
 	for DIR in $FILTERED_DIRS; do
 		stow --dotfiles --target="$HOME" "$DIR" &>>/dev/null
 	done
 
-	echo -e "$M_NOTE - Copying fonts..."
+	echo -e "$NOTE - Copying fonts..."
 
 	mkdir -p "$HOME"/.local/share/fonts/ >>/dev/null
 
 	stow --dotfiles --target="$HOME" fonts &>>/dev/null
 
-	echo -e "$M_NOTE - Refreshing font cache..."
+	echo -e "$NOTE - Refreshing font cache..."
 
 	fc-cache -f
 
-	echo -e "$M_NOTE - Updating git submodules..."
+	echo -e "$NOTE - Updating git submodules..."
 
   git submodule update --init
 fi
@@ -195,9 +192,13 @@ if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
 		echo -e "$M_NOTE - Changing default shell to fish..."
 		chsh -s "$(which zsh)"
 	else
-		echo -e "$M_NOTE - Fish is already the default shell!"
+		echo -e "$NOTE - Fish is already the default shell!"
 	fi
 fi
 
-echo -e "$M_NOTE - Installation was finished with success!"
-echo -e "$M_NOTE - You may close and reopen your terminal for changes to take full effect!"
+clear
+
+echo -e "$NOTE - Installation was finished with success!"
+echo -e "$NOTE - You may close and reopen your terminal for changes to take full effect!"
+
+sleep 1
