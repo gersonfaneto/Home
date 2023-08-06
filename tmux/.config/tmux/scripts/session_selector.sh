26b6a8fe5bdc8cc012306ecbb3cc8e549faf78c8
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-08-06 09:50:05 -0300

    refactor(tmux): Proper prompt for `session_selector` script.

--- tmux/.config/tmux/scripts/session_selector.sh
+++ tmux/.config/tmux/scripts/session_selector.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env fish
 
-tmux list-sessions -F '#{?session_attached,,#{session_name}}' | sed '/^$/d' | fzf-tmux -p | xargs tmux switch-client -t
+tmux list-sessions -F '#{?session_attached,,#{session_name}}' | sed '/^$/d' | \
+  fzf-tmux -p --  --prompt='Attach: ' | xargs tmux switch-client -t
