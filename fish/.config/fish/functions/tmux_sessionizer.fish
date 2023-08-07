c8f10bf31055d2bb20558fd69c8afdd369db6fd7
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-08-07 14:31:28 -0300

    feat(bin/fish): Minor adjustments to `tmux-sessionizer`.

--- fish/.config/fish/functions/tmux_sessionizer.fish
+++ fish/.config/fish/functions/tmux_sessionizer.fish
@@ -1,8 +1,10 @@
 function tmux_sessionizer
-  set -l targeted_directories "$HOME/Developer" "$HOME/Developer/GitHub" "$HOME"
-  set search_flags -mindepth 1 -maxdepth 1 -type d -not -path '*/.*'
+  set -l base_targets "$HOME/Developer" "$HOME/Developer/GitHub"
 
-  set selected (find $targeted_directories $search_flags | fzf --prompt='Sessionizer: ')
+  set -l found_directories (find $base_targets -mindepth 1 -maxdepth 1 -type d)
+  set -a found_directories "$HOME/Notes"
+
+  set selected (echo $found_directories | tr ' ' '\n' | fzf-tmux -p -- --prompt="Sessionizer: ")
 
   set is_attached (tmux list-sessions | grep attached | wc --lines)
 
