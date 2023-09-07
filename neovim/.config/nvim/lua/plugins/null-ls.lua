2326cb1d9ab57e1f6f1d3e191dc9e959b548e85d
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-09-07 18:36:36 -0300

    feat(neovim): Added formatter for `OCaml`.

--- neovim/.config/nvim/lua/plugins/null-ls.lua
+++ neovim/.config/nvim/lua/plugins/null-ls.lua
@@ -16,6 +16,7 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.autopep8,
+        null_ls.builtins.formatting.ocamlformat,
         null_ls.builtins.formatting.clang_format,
 
         null_ls.builtins.diagnostics.flake8,
