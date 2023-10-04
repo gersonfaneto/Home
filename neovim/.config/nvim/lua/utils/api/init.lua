17ee20e09b10b287d23c32e9ce7591dcf3365696
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-03 22:17:45 -0300

    feat(neovim): Added new module under `utils.api` for servers.

--- neovim/.config/nvim/lua/utils/api/init.lua
+++ neovim/.config/nvim/lua/utils/api/init.lua
@@ -2,5 +2,6 @@ local M = require("utils.api.api")
 
 M.mappings = require("utils.api.mappings")
 M.settings = require("utils.api.settings")
+M.servers = require("utils.api.servers")
 
 return M
---
+++ neovim/.config/nvim/lua/utils/api/servers.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+M.lsp = {
+  "bashls",
+  "clangd",
+  "cssls",
+  "emmet_ls",
+  "gopls",
+  "html",
+  "jdtls",
+  "lua_ls",
+  "ocamllsp",
+  "pyright",
+  "rust_analyzer",
+}
+
+M.linters = {
+  "eslint_d",
+  "pylint",
+  "shellcheck",
+}
+
+M.formatters = {
+  "black",
+  "clang-format",
+  "isort",
+  "ocamlformat",
+  "prettier",
+  "shfmt",
+  "stylua",
+}
+
+return M
