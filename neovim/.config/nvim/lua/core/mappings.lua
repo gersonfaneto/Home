5b2955fc28d07fd9d8f0fe23695015504dcd16ee
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-07 13:12:54 -0300

    feat(neovim): Added new mapping...

--- neovim/.config/nvim/lua/core/mappings.lua
+++ neovim/.config/nvim/lua/core/mappings.lua
@@ -160,6 +160,13 @@ api.mappings.bulk_register({
   },
   {
     mode = { "n" },
+    lhs = "X",
+    rhs = ":keeppatterns substitute/\\s*\\%#\\s*/\\r/e <bar> normal! ==^<CR>",
+    options = { silent = true, noremap = true },
+    description = "Split current line at the cursor position.",
+  },
+  {
+    mode = { "n" },
     lhs = "<leader>v",
     rhs = ":vsplit<CR>",
     options = { noremap = true, silent = true },
