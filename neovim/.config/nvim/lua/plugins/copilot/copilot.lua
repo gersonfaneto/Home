440b4de019f7d028a4250b76c67be2a33dd40546
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-09 18:41:20 -0300

    refactor(neovim): Moved `copilot` plugins into individual files.

---
+++ neovim/.config/nvim/lua/plugins/copilot/copilot.lua
@@ -0,0 +1,36 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = { "Copilot" },
+  config = function()
+    require("copilot").setup({
+      panel = {
+        enabled = true,
+        auto_refresh = true,
+        keymap = {
+          jump_next = "<C-j>",
+          jump_prev = "<C-k>",
+          accept = "<C-a>",
+          refresh = "r",
+          open = "<C-S-a>",
+        },
+        layout = {
+          position = "bottom",
+          ratio = 0.4,
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<C-a>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-j>",
+          prev = "<C-k>",
+          dismiss = "<C-e>",
+        },
+      },
+    })
+  end,
+}
