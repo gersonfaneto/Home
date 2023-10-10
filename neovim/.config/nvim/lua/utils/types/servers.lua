ba03e46baf863ef9737e331cd504f46ce39efe06
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-10 07:50:19 -0300

    feat(neovim): Added `svelte` and `tailwindcss` to `utils.types.servers`.

--- neovim/.config/nvim/lua/utils/types/servers.lua
+++ neovim/.config/nvim/lua/utils/types/servers.lua
@@ -1,34 +1,37 @@
 local M = {}
 
 M.lsp = {
-  "bashls",
-  "clangd",
-  "cssls",
-  "emmet_ls",
-  "gopls",
-  "html",
-  "jdtls",
-  "lua_ls",
-  "ocamllsp",
-  "pyright",
-  "rust_analyzer",
-  "tsserver",
+  "bashls", -- Bash
+  "clangd", -- C/C++
+  "cssls", -- CSS
+  "emmet_ls", -- HTML
+  "gopls", -- Go
+  "html", -- HTML
+  "jdtls", -- Java
+  "lua_ls", -- Lua
+  "ocamllsp", -- OCaml
+  "pyright", -- Python
+  "rust_analyzer", -- Rust
+  "svelte", -- Svelte
+  "tailwindcss", -- TailwindCSS
+  "tsserver", -- TypeScript / JavaScript
 }
 
 M.linters = {
-  "eslint_d",
-  "pylint",
-  "shellcheck",
+  "eslint_d", -- *
+  "pylint", -- Python
+  "shellcheck", -- Bash/Shell
 }
 
 M.formatters = {
-  "black",
-  "clang-format",
-  "isort",
-  "ocamlformat",
-  "prettier",
-  "shfmt",
-  "stylua",
+  "black", -- Python
+  "clang-format", -- C/C++
+  "google-java-format", -- Java
+  "isort", -- Python
+  "ocamlformat", -- OCaml
+  "prettier", -- *
+  "shfmt", -- Bash|Shell
+  "stylua", -- Lua
 }
 
 return M
