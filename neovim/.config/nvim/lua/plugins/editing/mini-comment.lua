7814e08e269ce4f5a4a7b7f54dc4a1760878ba1d
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-17 09:11:39 -0300

    feat(neovim): Updated mappings for `mini-comment`.

--- neovim/.config/nvim/lua/plugins/editing/mini-comment.lua
+++ neovim/.config/nvim/lua/plugins/editing/mini-comment.lua
@@ -2,8 +2,16 @@ return {
   {
     "echasnovski/mini.comment",
     event = { "BufReadPre", "BufNewFile" },
-    config = function(_, opts)
-      require("mini.comment").setup(opts)
+    config = function()
+      local mini_comment = require("mini.comment")
+
+      mini_comment.setup({
+        mappings = {
+          comment = "<C-_>",
+          comment_line = "<C-_>",
+          textobject = "<C-_>",
+        },
+      })
     end,
   },
 }
