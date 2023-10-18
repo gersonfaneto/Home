1ee6fc226d2472e019d7034cd639e9c6add0e648
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-18 19:56:14 -0300

    feat(neovim): Added new plugin - `glepnir/lspsaga.nvim`.

---
+++ neovim/.config/nvim/lua/plugins/coding/lspsaga-nvim.lua
@@ -0,0 +1,19 @@
+return {
+  "glepnir/lspsaga.nvim",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    local lspsaga = require("lspsaga")
+
+    lspsaga.setup({
+      move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+      finder_action_keys = {
+        open = "<CR>",
+      },
+      definition_action_keys = {
+        edit = "<CR>",
+      },
+    })
+  end,
+}
