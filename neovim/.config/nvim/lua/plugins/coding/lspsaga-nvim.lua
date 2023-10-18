9712d2eba4ff0111c2b5720d4a15149a6ff271e4
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-18 20:12:38 -0300

    feat(neovim): Updated settings & mappings - `glepnir/lspsaga.nvim`.

--- neovim/.config/nvim/lua/plugins/coding/lspsaga-nvim.lua
+++ neovim/.config/nvim/lua/plugins/coding/lspsaga-nvim.lua
@@ -4,15 +4,36 @@ return {
     "nvim-treesitter/nvim-treesitter",
   },
   config = function()
+    local base = require("utils.base")
+
     local lspsaga = require("lspsaga")
 
     lspsaga.setup({
-      move_in_saga = { prev = "<C-k>", next = "<C-j>" },
+      symbol_in_winbar = {
+        enable = false,
+      },
+      breadcrumbs = {
+        enable = false,
+      },
+      move_in_saga = {
+        prev = "<C-k>",
+        next = "<C-j>",
+      },
       finder_action_keys = {
         open = "<CR>",
       },
       definition_action_keys = {
         edit = "<CR>",
+      },
+    })
+
+    base.mappings.bulk_register({
+      {
+        mode = { "n" },
+        lhs = "<leader>to",
+        rhs = ":Lspsaga winbar_toggle<CR>",
+        options = { noremap = true, silent = true },
+        description = "LSP Saga - Toggle bread crumbs.",
       },
     })
   end,
