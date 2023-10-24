796322f3ff0764a3b88b2ba06544dec316fa5130
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-24 17:48:36 -0300

    feat(neovim): Improved options for previewing `Markdown` files.

--- neovim/.config/nvim/after/ftplugin/markdown.lua
+++ neovim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,61 +1,42 @@
-local jobid = 0
-
-local function start_server()
-  if vim.fn.executable("marp") ~= 1 then
-    print("Marp: Marp CLI executable not found.")
-    return
-  end
-
-  if jobid ~= 0 then
-    vim.fn.jobstop(jobid)
-    return
-  end
-
-  local port = os.getenv("PORT")
-  if port == nil then
-    port = "8005"
-    vim.fn.setenv("PORT", port)
-  end
-
-  print("Marp: Started server on http://localhost:" .. port .. ".")
-  jobid = vim.fn.jobstart({ "marp", "--server", "--preview", "--html", vim.fn.getcwd() }, {
-    on_exit = function(_, code)
-      jobid = 0
-      if code ~= 143 then
-        print("marp: exit", code)
-      end
-    end,
-  })
-end
-
-local function stop_server()
-  vim.fn.jobstop(jobid)
-  jobid = 0
-  print("Marp: Server stopped.")
-end
-
-vim.api.nvim_create_user_command("MarpStartServer", start_server, {})
-vim.api.nvim_create_user_command("MarpStopServer", stop_server, {})
-
 local base = require("utils.base")
+local plugins = require("utils.plugins")
 
 local M = {}
 
-base.mappings.bulk_register({
-  {
-    mode = { "n" },
-    lhs = "<leader>sm",
-    rhs = ":MarpStartServer<CR>",
-    options = { noremap = true, silent = true },
-    description = "Marp: Start server.",
-  },
-  {
-    mode = { "n" },
-    lhs = "<leader>Sm",
-    rhs = ":MarpStopServer<CR>",
-    options = { noremap = true, silent = true },
-    description = "Marp: Stop server.",
-  },
-})
+if plugins.marp.IsMarp() then
+  base.mappings.bulk_register({
+    {
+      mode = { "n" },
+      lhs = "<leader>sp",
+      rhs = plugins.marp.StartServer,
+      options = { noremap = true, silent = true },
+      description = "Marp: Start Preview.",
+    },
+    {
+      mode = { "n" },
+      lhs = "<leader>ep",
+      rhs = plugins.marp.StopServer,
+      options = { noremap = true, silent = true },
+      description = "Marp: End Preview.",
+    },
+  })
+else
+  base.mappings.bulk_register({
+    {
+      mode = { "n" },
+      lhs = "<leader>sp",
+      rhs = ":MarkdownPreview<CR>",
+      options = { noremap = true, silent = true },
+      description = "Markdown: Start Preview.",
+    },
+    {
+      mode = { "n" },
+      lhs = "<leader>ep",
+      rhs = ":MarkdownPreviewStop<CR>",
+      options = { noremap = true, silent = true },
+      description = "Markdown: End Preview.",
+    },
+  })
+end
 
 return M
--- neovim/.config/nvim/lua/plugins/tooling/markdown-preview.lua
+++ neovim/.config/nvim/lua/plugins/tooling/markdown-preview.lua
@@ -5,17 +5,7 @@ return {
     ft = { "markdown" },
     build = "cd app && npm install",
     config = function()
-      local base = require("utils.base")
-
       vim.g.mkdp_filetypes = { "markdown" }
-
-      base.mappings.register({
-        mode = { "n" },
-        lhs = "<leader>mp",
-        rhs = ":MarkdownPreviewToggle<CR>",
-        options = { noremap = true, silent = true },
-        description = "Toggle markdown preview.",
-      })
     end,
   },
 }
