50fe49d4fef1711b68df1074ef80463e2d374e58
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-24 17:47:13 -0300

    feat(neovim): Added `buffers` and `files` to `utils.base`.

---
+++ neovim/.config/nvim/lua/utils/base/files.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+--- Check if `filetype` has `dst`.
+--- @param dst string Destination file type.
+--- @param filetype? string File type to be checked, default *filetype* of current buffer.
+--- @return boolean result True if `filetype` has `dst`.
+function M.has_filetype(dst, filetype)
+  filetype = filetype or vim.bo.filetype
+
+  if not filetype then
+    return false
+  end
+
+  return vim.tbl_contains(vim.split(filetype, "%."), dst)
+end
+
+return M
--- neovim/.config/nvim/lua/utils/base/init.lua
+++ neovim/.config/nvim/lua/utils/base/init.lua
@@ -1,5 +1,7 @@
 local M = require("utils.base.base")
 
+M.files = require("utils.base.files")
+M.buffers = require("utils.base.buffers")
 M.mappings = require("utils.base.mappings")
 
 return M
