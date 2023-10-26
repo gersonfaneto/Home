3fd3cb898aa59f1b83c0b86e1fac75e813a0a0fe
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-26 13:42:40 -0300

    fix(neovim): Properly setup `config` for `akinsho/flutter-tools.nvim`.

--- neovim/.config/nvim/lua/plugins/coding/extras/flutter-tools.lua
+++ neovim/.config/nvim/lua/plugins/coding/extras/flutter-tools.lua
@@ -9,17 +9,5 @@ return {
     "nvim-lua/plenary.nvim",
     "stevearc/dressing.nvim",
   },
-  config = function()
-    local base = require("utils.base")
-
-    base.mappings.register({
-      mode = { "n" },
-      lhs = "<leader>fl",
-      rhs = ":Telescope flutter commands<CR>",
-      options = { noremap = true, silent = true },
-      description = "Open Telescope Flutter Commands.",
-    })
-
-    return true
-  end,
+  config = true,
 }
