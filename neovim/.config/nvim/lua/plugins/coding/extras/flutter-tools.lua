7a7bf704eebf74928fe9da939970621baedaae4c
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-26 14:19:24 -0300

    feat(neovim): Updated settings for `akinsho/flutter-tools.nvim`.

--- neovim/.config/nvim/lua/plugins/coding/extras/flutter-tools.lua
+++ neovim/.config/nvim/lua/plugins/coding/extras/flutter-tools.lua
@@ -9,5 +9,25 @@ return {
     "nvim-lua/plenary.nvim",
     "stevearc/dressing.nvim",
   },
-  config = true,
+  config = function()
+    local flutter_tools = require("flutter-tools")
+
+    flutter_tools.setup({
+      lsp = {
+        color = {
+          enabled = true,
+          background = true,
+          background_color = { r = 19, g = 17, b = 24 },
+          virtual_text = false,
+        },
+        settings = {
+          showTodos = false,
+          renameFilesWithClasses = "always",
+          updateImportsOnRename = true,
+          completeFunctionCalls = true,
+          lineLength = 100,
+        },
+      },
+    })
+  end,
 }
