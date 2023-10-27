b73731cf98bb58fddb4c4faf015706d63c9356f0
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-27 08:47:31 -0300

    feat(neovim): Minor tweaks on `folke/noice.nvim` settings.

--- neovim/.config/nvim/lua/plugins/viewing/noice-nvim.lua
+++ neovim/.config/nvim/lua/plugins/viewing/noice-nvim.lua
@@ -27,8 +27,8 @@ return {
         },
       },
       presets = {
-        bottom_search = true,
-        -- command_palette = true,
+        bottom_search = false,
+        command_palette = false,
         long_message_to_split = true,
         inc_rename = false,
         lsp_doc_border = false,
