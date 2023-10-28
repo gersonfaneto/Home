b9768c7239d695e61e219f4819658e3d31237d73
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-28 19:16:10 -0300

    feat(neovim): Updated list of `servers`, `linters` and `formatters`.

--- neovim/.config/nvim/lua/utils/types/servers.lua
+++ neovim/.config/nvim/lua/utils/types/servers.lua
@@ -1,37 +1,40 @@
 local M = {}
 
 M.lsp = {
-  "bashls", -- Bash
-  "clangd", -- C/C++
-  "cssls", -- CSS
-  "emmet_ls", -- HTML
-  "gopls", -- Go
-  "html", -- HTML
-  "jdtls", -- Java
-  "lua_ls", -- Lua
-  "ocamllsp", -- OCaml
-  "pyright", -- Python
+  "bashls",        -- Bash
+  "clangd",        -- C/C++
+  "cssls",         -- CSS
+  "emmet_ls",      -- HTML
+  "gopls",         -- Go
+  "html",          -- HTML
+  "jdtls",         -- Java
+  "lua_ls",        -- Lua
+  "ocamllsp",      -- OCaml
+  "pyright",       -- Python
   "rust_analyzer", -- Rust
-  "svelte", -- Svelte
-  "tailwindcss", -- TailwindCSS
-  "tsserver", -- TypeScript / JavaScript
+  "svelte",        -- Svelte
+  "tailwindcss",   -- TailwindCSS
+  "texlab",        -- LaTeX
+  "tsserver",      -- TypeScript / JavaScript
 }
 
 M.linters = {
-  "eslint_d", -- *
-  "pylint", -- Python
+  "vale",       -- LaTeX
+  "eslint_d",   -- *
+  "pylint",     -- Python
   "shellcheck", -- Bash/Shell
 }
 
 M.formatters = {
-  "black", -- Python
-  "clang-format", -- C/C++
+  "latexindent",        -- LaTeX
+  "black",              -- Python
+  "clang-format",       -- C/C++
   "google-java-format", -- Java
-  "isort", -- Python
-  "ocamlformat", -- OCaml
-  "prettier", -- *
-  "shfmt", -- Bash|Shell
-  "stylua", -- Lua
+  "isort",              -- Python
+  "ocamlformat",        -- OCaml
+  "prettier",           -- *
+  "shfmt",              -- Bash|Shell
+  "stylua",             -- Lua
 }
 
 return M
