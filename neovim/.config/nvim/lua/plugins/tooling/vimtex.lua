a5a0fba4b5e9622f8e72552d88ac03e494a0e79e
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-10-28 19:17:17 -0300

    feat(neovim): Updated settings for `lervag/vimtex`.

--- neovim/.config/nvim/lua/plugins/tooling/vimtex.lua
+++ neovim/.config/nvim/lua/plugins/tooling/vimtex.lua
@@ -6,8 +6,22 @@ return {
     "plaintext",
   },
   config = function()
+    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_quickfix_mode = 0
+
+    vim.g.vimtex_mappings_enabled = 0
+    vim.g.vimtex_indent_enabled = 0
+    vim.g.vimtex_syntax_enabled = 0
+
     vim.g.vimtex_compiler_latexmk = {
       build_dir = "out",
+    }
+
+    vim.g.vimtex_log_ignore = {
+      "Underfull",
+      "Overfull",
+      "specifier changed to",
+      "Token not allowed in a PDF string",
     }
   end,
 }
