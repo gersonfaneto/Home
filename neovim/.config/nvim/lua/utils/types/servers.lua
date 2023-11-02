a6745594c49ff428cf89a66ee7aeb43f9e4147ca
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-11-02 18:43:42 -0300

    feat(neovim): Added configurations for `dartls`.

--- neovim/.config/nvim/lua/utils/types/servers.lua
+++ neovim/.config/nvim/lua/utils/types/servers.lua
@@ -1,39 +1,44 @@
 local M = {}
 
 M.lsp = {
-  "bashls",        -- Bash
-  "clangd",        -- C/C++
-  "cssls",         -- CSS
-  "emmet_ls",      -- HTML
-  "gopls",         -- Go
-  "html",          -- HTML
-  "jdtls",         -- Java
-  "lua_ls",        -- Lua
-  "ocamllsp",      -- OCaml
-  "pyright",       -- Python
-  "rust_analyzer", -- Rust
-  "svelte",        -- Svelte
-  "tailwindcss",   -- TailwindCSS
-  "texlab",        -- LaTeX
-  "tsserver",      -- TypeScript / JavaScript
+  base = {
+    "bashls", -- Bash
+    "clangd", -- C/C++
+    "cssls", -- CSS
+    "emmet_ls", -- HTML
+    "gopls", -- Go
+    "html", -- HTML
+    "jdtls", -- Java
+    "lua_ls", -- Lua
+    "ocamllsp", -- OCaml
+    "pyright", -- Python
+    "rust_analyzer", -- Rust
+    "svelte", -- Svelte
+    "tailwindcss", -- TailwindCSS
+    "texlab", -- LaTeX
+    "tsserver", -- TypeScript / JavaScript
+  },
+  extras = {
+    "dartls", -- Dart
+  },
 }
 
 M.linters = {
-  "eslint_d",   -- *
-  "pylint",     -- Python
+  "eslint_d", -- TypeScript / JavaScript
+  "pylint", -- Python
   "shellcheck", -- Bash/Shell
 }
 
 M.formatters = {
-  "latexindent",        -- LaTeX
-  "black",              -- Python
-  "clang-format",       -- C/C++
+  "latexindent", -- LaTeX
+  "black", -- Python
+  "clang-format", -- C/C++
   "google-java-format", -- Java
-  "isort",              -- Python
-  "ocamlformat",        -- OCaml
-  "prettier",           -- *
-  "shfmt",              -- Bash|Shell
-  "stylua",             -- Lua
+  "isort", -- Python
+  "ocamlformat", -- OCaml
+  "prettier", -- *
+  "shfmt", -- Bash|Shell
+  "stylua", -- Lua
 }
 
 return M
