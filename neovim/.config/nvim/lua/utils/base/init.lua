03a81810d14402b1bad15b887d8c27c3d5222c4c
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-11-04 11:09:49 -0300

    feat(neovim): Added new submodule `tables` to `utils.base`.

--- neovim/.config/nvim/lua/utils/base/init.lua
+++ neovim/.config/nvim/lua/utils/base/init.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 M.files = require("utils.base.files")
+M.tables = require("utils.base.tables")
 M.buffers = require("utils.base.buffers")
 M.editing = require("utils.base.editing")
 M.mappings = require("utils.base.mappings")
