361a41f9bd96aa72e57aad79582c2ff51cdf3dcb
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-11-08 10:06:40 -0300

    refactor(neovim): Minor tweaks on `neovim/nvim-lspconfig`.

--- neovim/.config/nvim/lua/minimal/plugins/coding/nvim-lspconfig.lua
+++ neovim/.config/nvim/lua/minimal/plugins/coding/nvim-lspconfig.lua
@@ -14,93 +14,6 @@ return {
 
     local icons = interface.icons.get("diagnostics")
 
-    local on_attach = function(_, bufnr)
-      local opts = { noremap = true, silent = true, buffer = bufnr }
-
-      base.mappings.bulk_register({
-        {
-          mode = { "n", "v" },
-          lhs = "<leader>la",
-          rhs = ":Lspsaga code_action<CR>",
-          options = opts,
-          description = "Show code actions.",
-        },
-        {
-          mode = { "n" },
-          lhs = "<leader>rn",
-          rhs = ":Lspsaga rename<CR>",
-          options = opts,
-          description = "Rename symbol under cursor.",
-        },
-        {
-          mode = { "n" },
-          lhs = "K",
-          rhs = ":Lspsaga hover_doc<CR>",
-          options = opts,
-          description = "Show help information.",
-        },
-        {
-          mode = { "n" },
-          lhs = "gr",
-          rhs = ":Lspsaga finder ref<CR>",
-          options = opts,
-          description = "Go to references.",
-        },
-        {
-          mode = { "n" },
-          lhs = "gi",
-          rhs = ":Lspsaga finder imp<CR>",
-          options = opts,
-
-          description = "Go to implementations.",
-        },
-        {
-          mode = { "n" },
-          lhs = "gd",
-          rhs = ":Lspsaga peek_definition<CR>",
-          options = opts,
-          description = "Peek type definition.",
-        },
-        {
-          mode = { "n" },
-          lhs = "gD",
-          rhs = ":Lspsaga goto_definition<CR>",
-          options = opts,
-          description = "Go to type definition.",
-        },
-        {
-          mode = { "n" },
-          lhs = "<leader>ld",
-          rhs = ":Lspsaga show_line_diagnostics<CR>",
-          options = opts,
-          description = "Show current line diagnostics.",
-        },
-        {
-          mode = { "n" },
-          lhs = "<leader>cd",
-          rhs = ":Lspsaga show_cursor_diagnostics<CR>",
-          options = opts,
-          description = "Show diagnostics under cursor.",
-        },
-        {
-          mode = { "n" },
-          lhs = "[d",
-          rhs = ":Lspsaga diagnostic_jump_prev<CR>",
-          options = opts,
-          description = "Jump to previous diagnostic.",
-        },
-        {
-          mode = { "n" },
-          lhs = "]d",
-          rhs = ":Lspsaga diagnostic_jump_next<CR>",
-          options = opts,
-          description = "Jump to next diagnostic.",
-        },
-      })
-    end
-
-    local capabilities = plugins.lsp.capabilities.default_capabilities()
-
     local signs = {
       Error = icons.BoldError,
       Warn = icons.BoldWarning,
@@ -133,24 +46,26 @@ return {
       types.servers.extra_languages,
     })
 
-    for _, server in pairs(servers) do
-      local has_settings, extras = pcall(require, "minimal.plugins.coding.settings." .. server)
+    local on_attach = plugins.lsp.attach.on_attach()
+    local capabilities = plugins.lsp.capabilities.default_capabilities()
 
+    for _, server in pairs(servers) do
       if server == "jdtls" then
         goto continue
       end
 
+      local has_settings, extras = pcall(require, "minimal.plugins.coding.settings." .. server)
+
+      local settings = {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      }
+
       if has_settings then
-        lspconfig[server].setup(base.tables.merge({
-          capabilities = capabilities,
-          on_attach = on_attach,
-        }, extras))
-      else
-        lspconfig[server].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
-        })
+        settings = base.tables.merge(extras, settings)
       end
+
+      lspconfig[server].setup(settings)
 
       ::continue::
     end
