76fc93d57094a5c26ccbc7c8c55678c5f6d9cccd
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-11-08 19:50:04 -0300

    feat(neovim): Added `is_file` and `is_directory` to `utils.base.files`.

--- neovim/.config/nvim/lua/minimal/utils/base/files.lua
+++ neovim/.config/nvim/lua/minimal/utils/base/files.lua
@@ -3,7 +3,7 @@ local M = {}
 ---Check if `filetype` has `dst`.
 ---@param dst string Destination file type.
 ---@param filetype? string File type to be checked, default *filetype* of current buffer.
----@return boolean result True if `filetype` has `dst`.
+---@return boolean result `true` if `filetype` has `dst`.
 function M.has_filetype(dst, filetype)
   filetype = filetype or vim.bo.filetype
 
@@ -12,6 +12,24 @@ function M.has_filetype(dst, filetype)
   end
 
   return vim.tbl_contains(vim.split(filetype, "%."), dst)
+end
+--
+---Checks whether a given path exists and is a file.
+---@param path string Path to check.
+---@return boolean
+function M.is_file(path)
+  local stat = vim.loop.fs_stat(path)
+
+  return stat and stat.type == "file" or false
+end
+
+---Checks whether a given path exists and is a directory.
+---@param path string Path to check.
+---@return boolean
+function M.is_directory(path)
+  local stat = vim.loop.fs_stat(path)
+
+  return stat and stat.type == "directory" or false
 end
 
 return M
