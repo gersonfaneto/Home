9828fc28e8b27a602645209fcfaaf2a49bdf67e9
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-11-09 13:13:00 -0300

    feat(neovim): Updated mappings for `nvim-dbee`.

--- neovim/.config/nvim/lua/minimal/core/after/mappings/dbee.lua
+++ neovim/.config/nvim/lua/minimal/core/after/mappings/dbee.lua
@@ -1,4 +1,5 @@
 local base = require("minimal.utils.base")
+local plugins = require("minimal.utils.plugins")
 
 local M = {}
 
@@ -7,31 +8,55 @@ base.mappings.bulk_register({
     mode = { "n" },
     lhs = "<leader>Do",
     rhs = ":lua require('dbee').open()<CR>",
-    description =  "Open UI.",
+    description = "Open UI.",
   },
   {
     mode = { "n" },
     lhs = "<leader>Dc",
     rhs = ":lua require('dbee').close()<CR>",
-    description =  "Close UI.",
+    description = "Close UI.",
   },
   {
     mode = { "n" },
     lhs = "<leader>Dt",
     rhs = ":lua require('dbee').toggle()<CR>",
-    description =  "Toggle UI.",
+    description = "Toggle UI.",
   },
   {
     mode = { "n" },
     lhs = "<leader>Dn",
     rhs = ":lua require('dbee').next()<CR>",
-    description =  "Go to next result page.",
+    description = "Go to next result page.",
   },
   {
     mode = { "n" },
     lhs = "<leader>Dp",
     rhs = ":lua require('dbee').prev()<CR>",
-    description =  "Go to previous result page.",
+    description = "Go to previous result page.",
+  },
+  {
+    mode = { "n" },
+    lhs = "<leader>De",
+    rhs = function()
+      plugins.dbee.execute()
+    end,
+    description = "Quicly execute a query.",
+  },
+  {
+    mode = { "n" },
+    lhs = "<leader>Ds",
+    rhs = function()
+      plugins.dbee.save()
+    end,
+    description = "Save the query output.",
+  },
+  {
+    mode = { "v" },
+    lhs = "<leader>Ds",
+    rhs = function()
+      plugins.dbee.save(true)
+    end,
+    description = "Save the query output.",
   },
 }, { options = { silent = true, noremap = true }, prefix = "DB: " })
 
