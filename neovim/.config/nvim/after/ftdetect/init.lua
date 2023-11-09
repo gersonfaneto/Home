8fb5e6e0cd25a94bc169de622d8f15ec892e6d28
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-11-09 11:20:46 -0300

    refactor(neovim): `after/plugin/filetype` -> `after/ftdetect`.

--- neovim/.config/nvim/after/ftdetect/init.lua
+++ neovim/.config/nvim/after/ftdetect/init.lua
@@ -4,9 +4,16 @@ end
 
 vim.filetype.add({
   filename = {
+    [".clangd"] = "yaml",
+    [".clang-format"] = "yaml",
+    [".gitignore"] = "gitconfig",
+    ["go.sum"] = "gosum",
+    ["go.mod"] = "gomod",
     ["LICENSE"] = "license",
   },
   pattern = {
+    ["*.log"] = "log",
+    ["*_LOG"] = "log",
     [".*%.conf"] = "conf",
     [".*%.theme"] = "conf",
     ["^.env%..*"] = "bash",
