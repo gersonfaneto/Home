08e006933f565f950180ff53c41b5e3ddfaf9bd8
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-11-15 16:03:08 -0300

    refactor(neovim): Minor tweak on `vim-tmux-navigator` loading condition.

--- neovim/.config/nvim/lua/minimal/plugins/tooling/vim-tmux-navigator.lua
+++ neovim/.config/nvim/lua/minimal/plugins/tooling/vim-tmux-navigator.lua
@@ -1,9 +1,6 @@
 return {
   "christoomey/vim-tmux-navigator",
   event = "VeryLazy",
-  cond = function()
-    return os.getenv("TMUX") ~= nil
-  end,
   config = function()
     vim.g.tmux_navigator_no_mappings = 1
   end,
