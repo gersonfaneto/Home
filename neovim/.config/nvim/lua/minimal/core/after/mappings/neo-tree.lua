cbdd2e61ecbbe689330ab57559b5cccd8a98b12d
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-11-26 21:42:21 -0300

    feat(neovim): Updated/Added mappings for `neo-tree`.

--- neovim/.config/nvim/lua/minimal/core/after/mappings/neo-tree.lua
+++ neovim/.config/nvim/lua/minimal/core/after/mappings/neo-tree.lua
@@ -5,9 +5,15 @@ local M = {}
 base.mappings.bulk_register({
   {
     mode = { "n" },
-    lhs = "<C-p>",
+    lhs = "<C-f>",
     rhs = ":Neotree toggle float<CR>",
     description = "Toggle float.",
+  },
+  {
+    mode = { "n" },
+    lhs = "<C-s>",
+    rhs = ":Neotree toggle left<CR>",
+    description = "Toggle left.",
   },
 }, { options = { silent = true, noremap = true }, prefix = "NeoTree: " })
 
