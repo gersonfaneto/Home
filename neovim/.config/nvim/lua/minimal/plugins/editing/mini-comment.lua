86bb5fe3c046166dc825fee63d3140cedbf81dc2
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-12-01 09:26:34 -0300

    fix(neovim): Revert to default mappings of `mini-comment` plugin.

--- neovim/.config/nvim/lua/minimal/plugins/editing/mini-comment.lua
+++ neovim/.config/nvim/lua/minimal/plugins/editing/mini-comment.lua
@@ -7,13 +7,6 @@ return {
   config = function()
     local mini_comment = require("mini.comment")
 
-    mini_comment.setup({
-      mappings = {
-        comment = "<C-_>",
-        comment_line = "<C-_>",
-        comment_visual = "<C-_>",
-        textobject = "<C-_>",
-      },
-    })
+    mini_comment.setup({})
   end,
 }
