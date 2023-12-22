4e2a7fae27f4d351c31c455c098af3f6f05a8214
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-12-22 16:17:38 -0300

    feat(neovim): Updated mappings for `neorg`.

--- neovim/.config/nvim/lua/minimal/core/after/mappings/neorg.lua
+++ neovim/.config/nvim/lua/minimal/core/after/mappings/neorg.lua
@@ -5,12 +5,6 @@ local M = {}
 base.mappings.bulk_register({
   {
     mode = { "n" },
-    lhs = "<leader>nw",
-    rhs = ":Neorg workspace<CR>",
-    description = "Go to the default workspace - 'Notes'.",
-  },
-  {
-    mode = { "n" },
     lhs = "<leader>ni",
     rhs = ":Neorg index<CR>",
     description = "Open current workspace index.",
--- neovim/.config/nvim/lua/minimal/plugins/editing/neorg.lua
+++ neovim/.config/nvim/lua/minimal/plugins/editing/neorg.lua
@@ -18,7 +18,17 @@ return {
         ["core.keybinds"] = {
           config = {
             hook = function(keybinds)
+              local leader = keybinds.leader
+
               keybinds.remap_key("norg", "n", "<C-Space>", "<M-Space>")
+              keybinds.remap_key("norg", "n", leader .. "td", "<C-t><C-d>")
+              keybinds.remap_key("norg", "n", leader .. "tu", "<C-t><C-u>")
+              keybinds.remap_key("norg", "n", leader .. "tp", "<C-t><C-p>")
+              keybinds.remap_key("norg", "n", leader .. "th", "<C-t><C-h>")
+              keybinds.remap_key("norg", "n", leader .. "ta", "<C-t><C-a>")
+              keybinds.remap_key("norg", "n", leader .. "tr", "<C-t><C-r>")
+              keybinds.remap_key("norg", "n", leader .. "tc", "<C-t><C-t>")
+              keybinds.remap_key("norg", "n", leader .. "ti", "<C-t><C-i>")
             end,
           },
         },
