905a001fbf14566d24f438c1b4f2ae3632cb3703
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-12-23 08:12:54 -0300

    feat(neovim): Updated mappings for `Diffview`.

--- neovim/.config/nvim/lua/minimal/core/after/mappings/diffview.lua
+++ neovim/.config/nvim/lua/minimal/core/after/mappings/diffview.lua
@@ -21,6 +21,12 @@ base.mappings.bulk_register({
     rhs = ":DiffviewFileHistory<CR>",
     description = "Open diff history.",
   },
+  {
+    mode = { "n" },
+    lhs = "<leader>df",
+    rhs = ":DiffviewFileHistory %<CR>",
+    description = "Open diff history for current file.",
+  },
 }, { options = { noremap = true, silent = true }, prefix = "Diffview: " })
 
 return M
