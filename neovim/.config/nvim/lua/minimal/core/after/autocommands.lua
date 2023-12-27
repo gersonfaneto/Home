ecad4acde517936c1177dd3a876f2343cc811fcb
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-12-27 11:41:57 -0300

    feat(neovim): Automatically replace `QuickFix` with `Trouble`.

--- neovim/.config/nvim/lua/minimal/core/after/autocommands.lua
+++ neovim/.config/nvim/lua/minimal/core/after/autocommands.lua
@@ -45,6 +45,24 @@ if types.settings.transparent_background then
   })
 end
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "qf" },
+  callback = function()
+    local trouble = require("trouble")
+    if vim.fn.getloclist(0, { filewinid = 1 }).filewinid ~= 0 then
+      vim.defer_fn(function()
+        vim.cmd.lclose()
+        trouble.open("loclist")
+      end, 0)
+    else
+      vim.defer_fn(function()
+        vim.cmd.cclose()
+        trouble.open("quickfix")
+      end, 0)
+    end
+  end,
+})
+
 vim.api.nvim_create_autocmd("BufEnter", {
   command = "set spell",
   pattern = {
