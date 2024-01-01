76c74dc60c7f05402bc4fa5f261d2e73bb4b856a
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2023-12-31 23:00:44 -0300

    feat(neovim): Added plugin - `mini.starter`.

---
+++ neovim/.config/nvim/lua/minimal/core/plugins/viewing/mini-starter.lua
@@ -0,0 +1,70 @@
+return {
+  "echasnovski/mini.starter",
+  event = { "VimEnter" },
+  config = function()
+    local starter = require("mini.starter")
+
+    starter.setup({
+      autoopen = true,
+      evaluate_single = true,
+      silent = true,
+      header = ""
+        .. "Experience is the name everyone gives to their mistakes.\n"
+        .. "                                         -- Oscar Wilde",
+      footer = "",
+      query_updaters = "abcdefghijklmnopqrstuvwxyz0123456789.",
+      content_hooks = {
+        starter.gen_hook.adding_bullet(),
+        starter.gen_hook.aligning("center", "center"),
+      },
+      items = {
+        {
+          name = "New File",
+          action = "BufferCreate",
+          section = "Files",
+        },
+        {
+          name = "File Browser",
+          action = "Oil --float",
+          section = "Files",
+        },
+        {
+          name = "Recent Files",
+          action = "Telescope oldfiles",
+          section = "Files",
+        },
+        {
+          name = "Find Files",
+          action = function()
+            require("telescope.builtin").find_files({
+              -- This will include .gitignore files in the search results.
+              no_ignore = true,
+            })
+          end,
+          section = "Files",
+        },
+        {
+          name = "Search Text",
+          action = "Telescope live_grep",
+          section = "Search",
+        },
+        {
+          name = "Notes",
+          action = function()
+            require("telescope.builtin").find_files({
+              no_ignore = true,
+              prompt_title = " Notes ",
+              cwd = "$HOME/Notes",
+            })
+          end,
+          section = "Notes",
+        },
+        {
+          name = "Exit",
+          action = "qa",
+          section = "Exit",
+        },
+      },
+    })
+  end,
+}
