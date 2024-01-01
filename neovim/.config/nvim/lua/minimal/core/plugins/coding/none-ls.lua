40c4e2d065088d875870dfbaa82c4e918e819bfb
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-01 12:12:58 -0300

    refactor(neovim): Updated settings for plugin - `none-ls`.

--- neovim/.config/nvim/lua/minimal/core/plugins/coding/none-ls.lua
+++ neovim/.config/nvim/lua/minimal/core/plugins/coding/none-ls.lua
@@ -3,23 +3,9 @@ return {
   event = { "VeryLazy" },
   config = function()
     local types = require("minimal.utils.types")
+    local plugins = require("minimal.utils.plugins")
 
     local null_ls = require("null-ls")
-
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-    local on_attach = function(client, bufnr)
-      if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = augroup,
-          buffer = bufnr,
-          callback = function()
-            vim.lsp.buf.format({ async = false })
-          end,
-        })
-      end
-    end
 
     null_ls.setup({
       border = types.settings.transparent_floats and "double" or "none",
@@ -33,7 +19,9 @@ return {
 
         null_ls.builtins.diagnostics.pylint,
       },
-      on_attach = types.settings.format_on_save and on_attach or nil,
+      on_attach = function (client, bufnr)
+        plugins.lsp.formatting.on_attach(client, bufnr)
+      end
     })
   end,
 }
