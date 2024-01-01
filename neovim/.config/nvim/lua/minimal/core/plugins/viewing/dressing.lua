6c1c0e886e24ac0815185a110323ff45aafc850e
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-01 14:04:26 -0300

    feat(neovim): Updated options/settings for plugin - `dressing.nvim`.

--- neovim/.config/nvim/lua/minimal/core/plugins/viewing/dressing.lua
+++ neovim/.config/nvim/lua/minimal/core/plugins/viewing/dressing.lua
@@ -1,4 +1,21 @@
 return {
   "stevearc/dressing.nvim",
   event = { "VeryLazy" },
+  config = function()
+    local dressing = require("dressing")
+
+    dressing.setup({
+      input = {
+        relative = "win",
+        title_pos = "center",
+        default_prompt = "➤ ",
+      },
+      select = {
+        backend = { "builtin" },
+        builtin = {
+          relative = "win",
+        },
+      },
+    })
+  end,
 }
