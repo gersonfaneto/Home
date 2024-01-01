94f4ec72a208f4493e4fc7f91e4044ef662904e7
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-01 13:24:55 -0300

    feat(neovim): Cleaned up & Updated settings for plugin - `nvim-cmp`.

--- neovim/.config/nvim/lua/minimal/core/plugins/coding/nvim-cmp.lua
+++ neovim/.config/nvim/lua/minimal/core/plugins/coding/nvim-cmp.lua
@@ -33,10 +33,89 @@ return {
 
     require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/extras/snippets" } })
     cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      experimental = {
+        ghost_text = {
+          hl_group = "LspCodeLens",
+        },
+      },
+      window = {
+        completion = cmp.config.window.bordered({
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+        }),
+        documentation = cmp.config.window.bordered({
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+        }),
+      },
+      sources = {
+        { name = "luasnip" },
+        { name = "nvim_lua" },
+        { name = "nvim_lsp" },
+        { name = "path" },
+        { name = "emoji" },
+        { name = "buffer" },
+        { name = "crates" },
+      },
+      mapping = cmp_mapping.preset.insert({
+        ["<C-e>"] = cmp_mapping.abort(),
+        ["<C-c>"] = cmp_mapping.complete(),
+        ["<C-b>"] = cmp_mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp_mapping.scroll_docs(4),
+        ["<Up>"] = cmp_mapping(cmp_mapping.select_prev_item({ behavior = cmp_types.SelectBehavior.Select }), {
+          "i",
+        }),
+        ["<Down>"] = cmp_mapping(cmp_mapping.select_next_item({ behavior = cmp_types.SelectBehavior.Select }), {
+          "i",
+        }),
+        ["<Tab>"] = cmp_mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp_mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<CR>"] = cmp_mapping(function(fallback)
+          if cmp.visible() then
+            local confirm_opts = vim.deepcopy({
+              behavior = cmp_types.ConfirmBehavior.Replace,
+              select = false,
+            })
+
+            local is_insert_mode = function()
+              return vim.api.nvim_get_mode().mode:sub(1, 1) == "i"
+            end
+
+            if is_insert_mode() then
+              confirm_opts.behavior = cmp_types.ConfirmBehavior.Insert
+            end
+
+            if cmp.confirm(confirm_opts) then
+              return
+            end
+          end
+          fallback()
+        end),
+      }),
       formatting = {
         fields = { "kind", "abbr", "menu" },
         format = function(entry, vim_item)
           local max_width = 0
+
           if max_width ~= 0 and #vim_item.abbr > max_width then
             vim_item.abbr = string.sub(vim_item.abbr, 1, max_width - 1) .. icons.ui.Ellipsis
           end
@@ -70,104 +149,6 @@ return {
           return vim_item
         end,
       },
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      window = {
-        completion = cmp.config.window.bordered({
-          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-        }),
-        documentation = cmp.config.window.bordered({
-          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-        }),
-      },
-      sources = {
-        {
-          name = "nvim_lsp",
-          entry_filter = function(entry, ctx)
-            local kind = require("cmp.types.lsp").CompletionItemKind[entry:get_kind()]
-            if kind == "Snippet" and ctx.prev_context.filetype == "java" then
-              return false
-            end
-            if kind == "Text" then
-              return false
-            end
-            return true
-          end,
-        },
-
-        { name = "crates" },
-        { name = "luasnip" },
-        { name = "nvim_lua" },
-        { name = "path" },
-        { name = "emoji" },
-        { name = "buffer" },
-      },
-      mapping = cmp_mapping.preset.insert({
-        ["<C-k>"] = cmp_mapping(cmp_mapping.select_prev_item(), { "i", "c" }),
-        ["<C-j>"] = cmp_mapping(cmp_mapping.select_next_item(), { "i", "c" }),
-        ["<Down>"] = cmp_mapping(cmp_mapping.select_next_item({ behavior = cmp_types.SelectBehavior.Select }), {
-          "i",
-        }),
-        ["<Up>"] = cmp_mapping(cmp_mapping.select_prev_item({ behavior = cmp_types.SelectBehavior.Select }), {
-          "i",
-        }),
-        ["<C-b>"] = cmp_mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp_mapping.scroll_docs(4),
-        ["<C-y>"] = cmp_mapping({
-          i = cmp_mapping.confirm({ behavior = cmp_types.ConfirmBehavior.Replace, select = false }),
-          c = function(fallback)
-            if cmp.visible() then
-              cmp.confirm({ behavior = cmp_types.ConfirmBehavior.Replace, select = false })
-            else
-              fallback()
-            end
-          end,
-        }),
-        ["<Tab>"] = cmp_mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp_mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<C-c>"] = cmp_mapping.complete(),
-        ["<C-e>"] = cmp_mapping.abort(),
-        ["<CR>"] = cmp_mapping(function(fallback)
-          if cmp.visible() then
-            local confirm_opts = vim.deepcopy({
-              behavior = cmp_types.ConfirmBehavior.Replace,
-              select = false,
-            })
-
-            local is_insert_mode = function()
-              return vim.api.nvim_get_mode().mode:sub(1, 1) == "i"
-            end
-
-            if is_insert_mode() then
-              confirm_opts.behavior = cmp_types.ConfirmBehavior.Insert
-            end
-
-            if cmp.confirm(confirm_opts) then
-              return
-            end
-          end
-          fallback()
-        end),
-      }),
     })
   end,
 }
