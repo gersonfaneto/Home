2be71ed375846c23f4d249e5dea110a666b0fe80
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-03 18:32:20 -0300

    fix(neovim): Build plugin `markdown-preview` without `npm` or `yarn`.

--- neovim/.config/nvim/lua/minimal/core/plugins/tooling/markdown-preview.lua
+++ neovim/.config/nvim/lua/minimal/core/plugins/tooling/markdown-preview.lua
@@ -1,6 +1,8 @@
 return {
   "iamcco/markdown-preview.nvim",
-  build = "cd app && npm install",
+  build = function()
+    vim.fn["mkdp#util#install"]()
+  end,
   event = {
     "BufReadPre *.md",
     "BufNewFile *.md",
