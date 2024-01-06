9260cc7351167019ac2340cd1a2a85eb2bdcaa8d
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-06 16:16:15 -0300

    feat(fish): Updated function `fzf_background`.

--- fish/.config/fish/functions/fzf_background.fish
+++ fish/.config/fish/functions/fzf_background.fish
@@ -2,11 +2,29 @@ function fzf_background
   echo "Dark Light" | tr ' ' '\n' | fzf | read line
 
   if test $line
-    set line (echo $line | tr '[:upper:]' '[:lower:]')
+    set BACKGROUND "$line"
+    set COLORS "Gruvbox Material - ($BACKGROUND)"
 
-    export BACKGROUND="$line"
+    ln -sf "$HOME/.config/fish/colors/$COLORS.fish" "$HOME/.config/fish/colors/current.fish"
+    source "$HOME/.config/fish/colors/current.fish"
 
-    colorizer $COLORSCHEME $BACKGROUND
+    ln -fs "$HOME/.config/tmux/colors/$COLORS.conf" "$HOME/.config/tmux/colors/current.conf"
+    tmux source "$HOME/.config/tmux/colors/current.conf"
+
+    set sessions (tmux list-sessions -F '#S')
+
+    for session in $sessions
+      set windows (tmux list-windows -t $session -F '#I')
+      for window in $windows
+        set panes (tmux list-panes -t $session:$window -F '#P')
+        for pane in $panes
+          tmux send-keys -t $session:$window.$pane "source $HOME/.config/fish/colors/current.fish && clear" ENTER
+        end
+      end
+    end
+
+    ln -fs "$HOME/.config/kitty/colors/$COLORS.conf" "$HOME/.config/kitty/colors/current.conf"
+    killall -SIGUSR1 kitty
   end
 
   commandline -f repaint
