203f31b0ef939729886690c02c0907581fe0b02e
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-06 16:42:19 -0300

    refactor(fish): Simplified/Cleaned up function `fzf_background`.

--- fish/.config/fish/functions/fzf_background.fish
+++ fish/.config/fish/functions/fzf_background.fish
@@ -1,31 +1,33 @@
 function fzf_background
-  echo "Dark Light" | tr ' ' '\n' | fzf | read line
+  switch $BACKGROUND
+    case Dark
+      set COLORS "Gruvbox Material - (Light)"
+    case Light
+      set COLORS "Gruvbox Material - (Dark)"
+    case '*'
+      echo "Something went very wrong..."
+  end
 
-  if test $line
-    set BACKGROUND "$line"
-    set COLORS "Gruvbox Material - ($BACKGROUND)"
+  ln -sf "$HOME/.config/fish/colors/$COLORS.fish" "$HOME/.config/fish/colors/current.fish"
+  source "$HOME/.config/fish/colors/current.fish"
 
-    ln -sf "$HOME/.config/fish/colors/$COLORS.fish" "$HOME/.config/fish/colors/current.fish"
-    source "$HOME/.config/fish/colors/current.fish"
+  ln -fs "$HOME/.config/tmux/colors/$COLORS.conf" "$HOME/.config/tmux/colors/current.conf"
+  tmux source "$HOME/.config/tmux/colors/current.conf"
 
-    ln -fs "$HOME/.config/tmux/colors/$COLORS.conf" "$HOME/.config/tmux/colors/current.conf"
-    tmux source "$HOME/.config/tmux/colors/current.conf"
+  set sessions (tmux list-sessions -F '#S')
 
-    set sessions (tmux list-sessions -F '#S')
-
-    for session in $sessions
-      set windows (tmux list-windows -t $session -F '#I')
-      for window in $windows
-        set panes (tmux list-panes -t $session:$window -F '#P')
-        for pane in $panes
-          tmux send-keys -t $session:$window.$pane "source $HOME/.config/fish/colors/current.fish && clear" ENTER
-        end
+  for session in $sessions
+    set windows (tmux list-windows -t $session -F '#I')
+    for window in $windows
+      set panes (tmux list-panes -t $session:$window -F '#P')
+      for pane in $panes
+        tmux send-keys -t $session:$window.$pane "source $HOME/.config/fish/colors/current.fish && clear" ENTER
       end
     end
-
-    ln -fs "$HOME/.config/kitty/colors/$COLORS.conf" "$HOME/.config/kitty/colors/current.conf"
-    killall -SIGUSR1 kitty
   end
+
+  ln -fs "$HOME/.config/kitty/colors/$COLORS.conf" "$HOME/.config/kitty/colors/current.conf"
+  killall -SIGUSR1 kitty
 
   commandline -f repaint
 end
