67562a2ee11630ae5d5635f527e65a098d7e738f
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-07 15:59:31 -0300

    feat(neovim): Restored plugin `copilot`...

--- neovim/.config/nvim/lua/minimal/utils/types/settings.lua
+++ neovim/.config/nvim/lua/minimal/utils/types/settings.lua
@@ -39,12 +39,12 @@ M.auto_remove_new_lines_comment = false
 M.inlay_hints = false
 ---Set to `true` to enable `virtual_text`, for displaying diagnostics inline.
 ---@type boolean
-M.virtual_text = false
----Set to `true` to enable formatting files when saved, provided by `nvimtools/none-ls.nvim`.
+M.virtual_text = true
+---Set to `false` to disable formatting files when saved, provided by `nvimtools/none-ls.nvim`.
 ---@type boolean
 M.format_on_save = false
----Set to `true` to enable A.I. enhanced coding features, provided by `zbirenbaum/copilot.lua`.
+---Set to `false` to disable A.I. enhanced coding features, provided by `zbirenbaum/copilot.lua`.
 ---@type boolean
-M.enhanced_coding = false
+M.enhanced_coding = true
 
 return require("minimal.utils.base.settings").extend_settings(M, "custom.settings")
