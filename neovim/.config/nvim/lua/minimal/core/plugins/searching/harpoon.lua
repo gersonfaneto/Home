70e3d68baa94446403b3770d991078ffd0bdc3ba
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-13 16:50:51 -0300

    feat(neovim): Updated `Harpoon` to new version.

--- neovim/.config/nvim/lua/minimal/core/plugins/searching/harpoon.lua
+++ neovim/.config/nvim/lua/minimal/core/plugins/searching/harpoon.lua
@@ -1,99 +1,84 @@
 local M = {
   "ThePrimeagen/harpoon",
   event = { "VeryLazy" },
+  branch = "harpoon2",
 }
 
 function M.config()
   local harpoon = require("harpoon")
 
-  harpoon.setup()
+  local function sync(evt, list)
+    local file = evt.file
+
+    local thing = vim.api.nvim_win_get_cursor(0)
+    local r, c = thing[1], thing[2]
+
+    for _, item in pairs(list.items) do
+      local relative = vim.loop.cwd() .. "/" .. item.value
+      if relative == file then
+        item.context = {
+          col = c,
+          row = r,
+        }
+      end
+    end
+  end
+
+  harpoon:setup({
+    default = {
+      save_on_toggle = true,
+      sync_on_ui_close = true,
+      BufLeave = sync,
+      VimLeavePre = sync,
+    },
+  })
 end
+
+M.helpers = {
+  open_menu = function()
+    local harpoon = require("harpoon")
+
+    harpoon.ui:toggle_quick_menu(harpoon:list())
+  end,
+  mark_file = function()
+    local harpoon = require("harpoon")
+
+    harpoon:list():append()
+
+    vim.notify("󰛢 Harpooned!")
+  end,
+  select_file = function(index)
+    local harpoon = require("harpoon")
+
+    harpoon:list():select(index)
+  end,
+}
 
 utils.base.mappings.bulk_register({
   {
     mode = { "n" },
-    lhs = "<leader>hf",
-    rhs = ":lua require('harpoon.mark').add_file()<CR>",
+    lhs = "<leader>mf",
+    rhs = M.helpers.mark_file,
     description = "Add current file to Harpoon.",
   },
   {
     mode = { "n" },
-    lhs = "<leader>hm",
-    rhs = ":lua require('harpoon.ui').toggle_quick_menu()<CR>",
-    description = "Open.",
-  },
-  {
-    mode = { "n" },
-    lhs = "<leader>1",
-    rhs = ":lua require('harpoon.ui').nav_file(1)<CR>",
-    description = "Go to file number 1.",
-  },
-  {
-    mode = { "n" },
-    lhs = "<leader>2",
-    rhs = ":lua require('harpoon.ui').nav_file(2)<CR>",
-    description = "Go to file number 2.",
-  },
-  {
-    mode = { "n" },
-    lhs = "<leader>3",
-    rhs = ":lua require('harpoon.ui').nav_file(3)<CR>",
-    description = "Go to file number 3.",
-  },
-  {
-    mode = { "n" },
-    lhs = "<leader>4",
-    rhs = ":lua require('harpoon.ui').nav_file(4)<CR>",
-    description = "Go to file number 4.",
-  },
-  {
-    mode = { "n" },
-    lhs = "<leader>5",
-    rhs = ":lua require('harpoon.ui').nav_file(5)<CR>",
-    description = "Go to file number 5.",
-  },
-  {
-    mode = { "n" },
-    lhs = "<leader>6",
-    rhs = ":lua require('harpoon.ui').nav_file(6)<CR>",
-    description = "Go to file number 6.",
-  },
-  {
-    mode = { "n" },
-    lhs = "<leader>7",
-    rhs = ":lua require('harpoon.ui').nav_file(7)<CR>",
-    description = "Go to file number 7.",
-  },
-  {
-    mode = { "n" },
-    lhs = "<leader>8",
-    rhs = ":lua require('harpoon.ui').nav_file(8)<CR>",
-    description = "Go to file number 8.",
-  },
-  {
-    mode = { "n" },
-    lhs = "<leader>9",
-    rhs = ":lua require('harpoon.ui').nav_file(9)<CR>",
-    description = "Go to file number 9.",
-  },
-  {
-    mode = { "n" },
-    lhs = "<leader>0",
-    rhs = ":lua require('harpoon.ui').nav_file(10)<CR>",
-    description = "Go to file number 10.",
-  },
-  {
-    mode = { "n" },
-    lhs = "<leader>,",
-    rhs = ":lua require('harpoon.ui').nav_prev()<CR>",
-    description = "Go to previous file.",
-  },
-  {
-    mode = { "n" },
-    lhs = "<leader>.",
-    rhs = ":lua require('harpoon.ui').nav_next()<CR>",
-    description = "Go to next file.",
+    lhs = "<leader>mm",
+    rhs = M.helpers.open_menu,
+    description = "Browser Harpoon.",
   },
 }, { options = { silent = true, noremap = true }, prefix = "Harpoon: " })
+
+for i = 1, 10 do
+  utils.base.mappings.register({
+    mode = { "n" },
+    lhs = "<leader>" .. i % 10,
+    rhs = function()
+      M.helpers.select_file(i)
+    end,
+    options = { silent = true, noremap = true },
+    description = "Select " .. i .. "th File.",
+  })
+end
 
 return M
