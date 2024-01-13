5ab8de0c160bd17227e3623b8701ed8271acda8a
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-12 21:45:56 -0300

    feat(neovim): Updated mappings for `ccc.nvim` plugin.

--- neovim/.config/nvim/lua/minimal/core/plugins/tooling/ccc.lua
+++ neovim/.config/nvim/lua/minimal/core/plugins/tooling/ccc.lua
@@ -1,6 +1,12 @@
 local M = {
   "uga-rosa/ccc.nvim",
-  cmd = { "CccPick" },
+  cmd = {
+    "CccPick",
+    "CccConvert",
+    "CccHighlighterToggle",
+    "CccHighlighterEnable",
+    "CccHighlighterDisable",
+  },
   opts = {
     highlighter = {
       auto_enable = true,
@@ -21,6 +27,18 @@ utils.base.mappings.bulk_register({
     lhs = "<leader>cp",
     rhs = ":CccPick<CR>",
     description = "Open Color Picker.",
+  },
+  {
+    mode = { "n" },
+    lhs = "<leader>ct",
+    rhs = ":CccHighlighterToggle<CR>",
+    description = "Toggle Color Highlighter.",
+  },
+  {
+    mode = { "n" },
+    lhs = "<leader>cc",
+    rhs = ":CccConvert<CR>",
+    description = "Convert Color.",
   },
 }, { options = { noremap = true, silent = true }, prefix = "CCC: " })
 
