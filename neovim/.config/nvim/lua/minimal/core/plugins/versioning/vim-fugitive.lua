980f0f8be2ecfa10ced3dc2b55a22cf7505f49b2
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-14 19:26:43 -0300

    feat(neovim): Added new plugin - `vim-rhubarb`.

--- neovim/.config/nvim/lua/minimal/core/plugins/versioning/vim-fugitive.lua
+++ neovim/.config/nvim/lua/minimal/core/plugins/versioning/vim-fugitive.lua
@@ -1,6 +1,12 @@
 local M = {
   "tpope/vim-fugitive",
-  cmd = "Git",
+  cmd = {
+    "Git",
+    "GBrowse",
+  },
+  dependencies = {
+    "tpope/vim-rhubarb",
+  },
 }
 
 utils.base.mappings.bulk_register({
@@ -9,6 +15,12 @@ utils.base.mappings.bulk_register({
     lhs = "<leader>gg",
     rhs = ":Git<CR>",
     description = "Open Fugitive.",
+  },
+  {
+    mode = { "n", "x" },
+    lhs = "<leader>gb",
+    rhs = ":GBrowse<CR>",
+    description = "Open File/Selection in Browser.",
   },
 }, { prefix = "Fugitive: ", options = { silent = true, noremap = true } })
 
