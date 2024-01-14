443af1c9c6aa884c08a56ae79926c7997b9b620e
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-14 20:23:57 -0300

    feat(neovim): Added new plugin - `toggleterm`.

---
+++ neovim/.config/nvim/lua/minimal/core/plugins/enhancements/toggleterm.lua
@@ -0,0 +1,31 @@
+local M = {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+}
+
+function M.config()
+  local toggleterm = require("toggleterm")
+
+  toggleterm.setup({
+    size = 20,
+    open_mapping = [[<leader>tt]],
+    shade_filetypes = {},
+    shade_terminals = true,
+    shading_factor = 2,
+    start_in_insert = true,
+    persist_size = false,
+    direction = "float",
+    close_on_exit = true,
+    shell = vim.o.shell,
+    float_opts = {
+      border = "curved",
+      winblend = 0,
+      highlights = {
+        border = "Normal",
+        background = "Normal",
+      },
+    },
+  })
+end
+
+return M
