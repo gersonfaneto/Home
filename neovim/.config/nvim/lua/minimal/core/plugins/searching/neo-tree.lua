146689196a216c5da6df3e81889b10dbe521899e
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-14 21:23:14 -0300

    feat(neovim): Added plugin - `neo-tree.nvim`.

---
+++ neovim/.config/nvim/lua/minimal/core/plugins/searching/neo-tree.lua
@@ -0,0 +1,118 @@
+local M = {
+  "nvim-neo-tree/neo-tree.nvim",
+  event = { "VeryLazy" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+}
+
+function M.config()
+  local neo_tree = require("neo-tree")
+
+  neo_tree.setup({
+    close_if_last_window = true,
+    popup_border_style = "single",
+    enable_git_status = true,
+    enable_modified_markers = true,
+    enable_diagnostics = true,
+    sort_case_insensitive = true,
+    default_component_configs = {
+      indent = {
+        with_markers = true,
+        with_expanders = true,
+      },
+      modified = {
+        symbol = " ",
+        highlight = "NeoTreeModified",
+      },
+      icon = {
+        folder_closed = "",
+        folder_open = "",
+        folder_empty = "",
+        folder_empty_open = "",
+      },
+      git_status = {
+        symbols = {
+          -- Change type
+          added = "",
+          deleted = "",
+          modified = "",
+          renamed = "",
+          -- Status type
+          untracked = "",
+          ignored = "",
+          unstaged = "",
+          staged = "",
+          conflict = "",
+        },
+      },
+    },
+    window = {
+      position = "float",
+      width = 35,
+    },
+    filesystem = {
+      use_libuv_file_watcher = true,
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_by_name = {
+          "node_modules",
+        },
+        never_show = {
+          ".DS_Store",
+          "thumbs.db",
+        },
+      },
+    },
+    event_handlers = {
+      {
+        event = "neo_tree_window_after_open",
+        handler = function(args)
+          if args.position == "left" or args.position == "right" then
+            vim.cmd("wincmd =")
+          end
+        end,
+      },
+      {
+        event = "neo_tree_window_after_close",
+        handler = function(args)
+          if args.position == "left" or args.position == "right" then
+            vim.cmd("wincmd =")
+          end
+        end,
+      },
+    },
+  })
+
+  M.handlers.register_mappings()
+end
+
+M.handlers = {
+  register_mappings = function()
+    utils.base.mappings.bulk_register({
+      {
+        mode = { "n" },
+        lhs = "<C-e><C-r>",
+        rhs = ":Neotree toggle right<CR>",
+        description = "Toggle the right window.",
+      },
+      {
+        mode = { "n" },
+        lhs = "<C-e><C-l>",
+        rhs = ":Neotree toggle left<CR>",
+        description = "Toggle the left window.",
+      },
+      {
+        mode = { "n" },
+        lhs = "<C-e><C-f>",
+        rhs = ":Neotree toggle float<CR>",
+        description = "Toggle the floating window.",
+      },
+    }, { prefix = "NeoTree: ", options = { silent = true, noremap = true } })
+  end,
+}
+
+return M
