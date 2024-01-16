42cfe82c01775c321e44479613c05fd5af0b8d09
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-15 21:16:10 -0300

    fix(neovim): Disable `nvim-treesitter-context` in some file types.

--- neovim/.config/nvim/lua/minimal/core/plugins/editor/nvim-treesitter.lua
+++ neovim/.config/nvim/lua/minimal/core/plugins/editor/nvim-treesitter.lua
@@ -110,6 +110,17 @@ function M.config()
   })
 end
 
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("disable_context", { clear = true }),
+  pattern = {
+    "norg",
+    "markdown",
+  },
+  callback = function()
+    vim.cmd("TSContextDisable")
+  end,
+})
+
 utils.base.mappings.bulk_register({
   {
     mode = { "n", "v" },
