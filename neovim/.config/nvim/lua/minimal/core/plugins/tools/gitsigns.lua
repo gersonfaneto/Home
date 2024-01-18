434ce1ae21b1be91eb0fe53fd39835053968f562
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-18 13:53:14 -0300

    feat(neovim): Updated mappings for some plugins...

--- neovim/.config/nvim/lua/minimal/core/plugins/tools/gitsigns.lua
+++ neovim/.config/nvim/lua/minimal/core/plugins/tools/gitsigns.lua
@@ -137,7 +137,7 @@ utils.base.mappings.bulk_register({
   },
   {
     mode = { "n" },
-    lhs = "[c",
+    lhs = "[h",
     rhs = function()
       if vim.wo.diff then
         return "[c"
@@ -151,7 +151,7 @@ utils.base.mappings.bulk_register({
   },
   {
     mode = { "n" },
-    lhs = "]c",
+    lhs = "]h",
     rhs = function()
       if vim.wo.diff then
         return "]c"
