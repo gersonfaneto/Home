1c75026452dd5155fe17a476d49c6f07c3f34aa7
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-20 13:41:47 -0300

    fix!(neovim): Breaking changes on `gx.nvim`.

--- neovim/.config/nvim/lua/minimal/core/plugins/others/gx.lua
+++ neovim/.config/nvim/lua/minimal/core/plugins/others/gx.lua
@@ -1,12 +1,29 @@
 local M = {
-  "chrishrb/gx.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
+  ["gx.nvim"] = {
+    "chrishrb/gx.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = { "Browse" },
+    opts = {},
+  },
+  helpers = {},
 }
 
-function M.config()
-  local gx = require("gx")
+M["gx.nvim"].init = function()
+  vim.g.netrw_nogx = 1
 
-  gx.setup({})
+  M.helpers.register_mappings()
 end
 
-return M
+M.helpers = {
+  register_mappings = function()
+    utils.base.mappings.register({
+      mode = { "n", "x" },
+      lhs = "gx",
+      rhs = ":Browse<CR>",
+      options = { silent = true, noremap = true },
+      description = "GX :: Browse.",
+    })
+  end,
+}
+
+return M["gx.nvim"]
