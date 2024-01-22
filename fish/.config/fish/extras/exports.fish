7f7f7dff01edfd35bd31c45c1d8e7e3be32b3f0d
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-01-21 21:40:51 -0300

    feat(fish): Updated `extras/exports` for `ghcup`.

--- fish/.config/fish/extras/exports.fish
+++ fish/.config/fish/extras/exports.fish
@@ -10,6 +10,18 @@ if test -d "$HOME/.local/scripts/"
   end
 end
 
+if test -d "$HOME/.ghcup/bin/"
+  if not contains $HOME/.ghcup/bin/ $PATH
+    export PATH="$PATH:$HOME/.ghcup/bin/"
+  end
+end
+
+if test -d "$HOME/.cabal/bin/"
+  if not contains $HOME/.cabal/bin/ $PATH
+    export PATH="$PATH:$HOME/.cabal/bin/"
+  end
+end
+
 if test -d "/usr/local/lib/"
   if not contains /usr/local/lib $LD_LIBRARY_PATH
     export LD_LIBRARY_PATH="/usr/local/lib/:$LD_LIBRARY_PATH"
