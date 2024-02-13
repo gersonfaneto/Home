dee5eaafd821fe2ebda3305c28c735b114870dcc
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-02-13 20:40:11 -0300

    feat(neovim): Updated mappings for `vim-fugitive`.

--- neovim/.config/nvim/lua/minimal/core/plugins/tools/vim-fugitive.lua
+++ neovim/.config/nvim/lua/minimal/core/plugins/tools/vim-fugitive.lua
@@ -11,61 +11,53 @@ local M = {
 }
 
 function M.init()
-  M.helpers.register_mappings()
-  M.helpers.register_autocmds()
+  utils.base.mappings.bulk_register({
+    {
+      mode = { "n" },
+      lhs = "<C-g><C-g>",
+      rhs = ":Git<CR>",
+      description = "Open Fugitive.",
+    },
+    {
+      mode = { "n", "x" },
+      lhs = "<C-g><C-b>",
+      rhs = ":GBrowse<CR>",
+      description = "Open File/Selection in Browser.",
+    },
+  }, { prefix = "Fugitive :: ", options = { silent = true, noremap = true } })
+
+  vim.api.nvim_create_autocmd("BufWinEnter", {
+    group = vim.api.nvim_create_augroup("FugitiveMappings", {}),
+    pattern = "*",
+    callback = function()
+      if vim.bo.ft ~= "fugitive" then
+        return
+      end
+
+      local bufnr = vim.api.nvim_get_current_buf()
+
+      utils.base.mappings.bulk_register({
+        {
+          mode = { "n" },
+          lhs = "<leader>p",
+          rhs = ":Git push<CR>",
+          description = "Push.",
+        },
+        {
+          mode = { "n" },
+          lhs = "<leader>P",
+          rhs = ":Git pull --rebase<CR>",
+          description = "Pull.",
+        },
+        {
+          mode = { "n" },
+          lhs = "<leader>t",
+          rhs = ":Git push -u origin ",
+          description = "Push with tracking.",
+        },
+      }, { prefix = "Fugitive :: ", options = { buffer = bufnr, silent = true, noremap = true } })
+    end,
+  })
 end
-
-M.helpers = {
-  register_mappings = function()
-    utils.base.mappings.bulk_register({
-      {
-        mode = { "n" },
-        lhs = "<leader>gg",
-        rhs = ":Git<CR>",
-        description = "Open Fugitive.",
-      },
-      {
-        mode = { "n", "x" },
-        lhs = "<leader>gb",
-        rhs = ":GBrowse<CR>",
-        description = "Open File/Selection in Browser.",
-      },
-    }, { prefix = "Fugitive :: ", options = { silent = true, noremap = true } })
-  end,
-  register_autocmds = function()
-    vim.api.nvim_create_autocmd("BufWinEnter", {
-      group = vim.api.nvim_create_augroup("FugitiveMappings", {}),
-      pattern = "*",
-      callback = function()
-        if vim.bo.ft ~= "fugitive" then
-          return
-        end
-
-        local bufnr = vim.api.nvim_get_current_buf()
-
-        utils.base.mappings.bulk_register({
-          {
-            mode = { "n" },
-            lhs = "<leader>p",
-            rhs = ":Git push<CR>",
-            description = "Push.",
-          },
-          {
-            mode = { "n" },
-            lhs = "<leader>P",
-            rhs = ":Git pull --rebase<CR>",
-            description = "Pull.",
-          },
-          {
-            mode = { "n" },
-            lhs = "<leader>t",
-            rhs = ":Git push -u origin ",
-            description = "Push with tracking.",
-          },
-        }, { prefix = "Fugitive :: ", options = { buffer = bufnr, silent = true, noremap = true } })
-      end,
-    })
-  end,
-}
 
 return M
