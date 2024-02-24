fa5d1b02a692275f9d8c53d798238a8932455019
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-02-24 10:11:34 -0300

    refactor(i3/kitty): Changed default theme.

--- kitty/.config/kitty/scripts/generate-fonts.sh
+++ kitty/.config/kitty/scripts/generate-fonts.sh
@@ -3,13 +3,17 @@
 FONTS_DIRECTORY="$HOME/.local/share/fonts/YADR/"
 KITTY_DIRECTORY="$HOME/.config/kitty"
 
-DEFAULT_FONT="Writer"
+DEFAULT_FONT="Iosevka Term"
 FONTS_FILE="$KITTY_DIRECTORY/extras/fonts.conf"
 
 AVAILABLE_FONTS=$(find "${FONTS_DIRECTORY}" -mindepth 1 -type d | xargs -I {} basename {})
 IFS=$'\n'
 
 FONTS=$(echo "$AVAILABLE_FONTS" | sort | cut -d "." -f 2)
+
+if [ ! -d "$KITTY_DIRECTORY/extras/" ]; then
+  mkdir -p "$KITTY_DIRECTORY/extras"
+fi
 
 if [ -f "$FONTS_FILE" ]; then
   echo -n "" > "$FONTS_FILE"
