cc2a1d5535d7c22be9e1c9c5efce0a8a995b953e
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-02-29 20:25:53 -0300

    feat(fish): Added `cargo` binaries to the `$PATH`.

--- fish/.config/fish/extras/exports.fish
+++ fish/.config/fish/extras/exports.fish
@@ -22,6 +22,12 @@ if test -d "$HOME/.cabal/bin/"
   end
 end
 
+if test -d "$HOME/.cargo/bin/"
+  if not contains $HOME/.cargo/bin/ $PATH
+    export PATH="$PATH:$HOME/.cargo/bin/"
+  end
+end
+
 if test -d "/usr/local/lib/"
   if not contains /usr/local/lib $LD_LIBRARY_PATH
     export LD_LIBRARY_PATH="/usr/local/lib/:$LD_LIBRARY_PATH"
