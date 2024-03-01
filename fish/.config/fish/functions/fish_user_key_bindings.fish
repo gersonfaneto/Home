84cc75bf39fd9edd6e7b4f7a179c4b788ba0d592
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-03-01 15:47:05 -0300

    refactor(fish|scripts): Moved `izathura` to `fish/functions`.

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -2,13 +2,12 @@ function fish_user_key_bindings
   bind \ef fzf_file
   bind \er fzf_history
   bind \ej fzf_directory
-
+  bind \ez fzf_zathura
+  
   bind \el "lf && commandline -f repaint"
   bind \eL "lfcd && commandline -f repaint"
   bind \eb btm
   bind \en nvim
-
-  bind \ez izathura
 
   bind \ek tmux_kill
   bind \ea tmux_attach
