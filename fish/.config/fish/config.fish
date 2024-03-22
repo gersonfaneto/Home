e86ab303cef22c62b0086d0fbce5dbcf6a5c12fd
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-03-22 20:39:41 -0300

    refactor!(fish): Split `extras` and `core` modules.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,10 +3,10 @@ set fish_greeting ""
 set -gx TERM xterm-256color
 set -gx EDITOR nvim
 
-source "$HOME/.config/fish/extras/alias.fish"
-source "$HOME/.config/fish/extras/exports.fish"
-source "$HOME/.config/fish/extras/functions.fish"
-source "$HOME/.config/fish/extras/enviroment.fish"
+source "$HOME/.config/fish/core/alias.fish"
+source "$HOME/.config/fish/core/exports.fish"
+source "$HOME/.config/fish/core/functions.fish"
+source "$HOME/.config/fish/core/enviroment.fish"
 
 # NOTE: This file is not tracked by git, should be used for local/temporary settings.
 if test -f "$HOME/.config/fish/extras/extras.fish"
