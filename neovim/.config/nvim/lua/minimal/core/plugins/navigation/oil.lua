c845c810f700827fdc673877e9d37d2d857f2694
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-03-25 07:41:04 -0300

    fix(neovim): `Oil` not initializing in `ssh` mode.

--- neovim/.config/nvim/lua/minimal/core/plugins/navigation/oil.lua
+++ neovim/.config/nvim/lua/minimal/core/plugins/navigation/oil.lua
@@ -9,8 +9,10 @@ local M = {
 
 function M.init()
   if vim.fn.argc() == 1 then
-    local stat = vim.loop.fs_stat(vim.fn.argv(0))
-    if stat and stat.type == "directory" then
+    local arg = vim.fn.argv(0)
+    local stat = vim.loop.fs_stat(arg)
+    if vim.startswith(arg, "oil-ssh://") or
+        stat and stat.type == "directory" then
       require("lazy").load({ plugins = { "oil.nvim" } })
     end
   end
