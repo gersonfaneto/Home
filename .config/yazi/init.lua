5543a1d6580184eb14e898f686a9d3bfe28eebe6
Gerson Ferreira <gersonfaneto.dev@gmail.com>
2024-08-11 19:39:41 -0300

    fix(yazi): Breaking changes on plugin - `full-border`.

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,10 +1,7 @@
-require("full-border"):setup()
+require("full-border"):setup({
+  type = ui.Border.ROUNDED,
+})
 
 function Status:render()
   return {}
-end
-
-local old_manager_render = Manager.render
-function Manager:render(area)
-  return old_manager_render(self, ui.Rect({ x = area.x, y = area.y, w = area.w, h = area.h + 1 }))
 end
